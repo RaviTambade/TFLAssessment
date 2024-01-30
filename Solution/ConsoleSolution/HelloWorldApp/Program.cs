08dddf20d434219034d4efd5e39de3f8478fa485
Ravi Tambade <ravi.tambade@transflower.in>
2024-01-30 11:30:51 +0530

    Basic C# Code

---
+++ Solution/ConsoleSolution/HelloWorldApp/Program.cs
@@ -0,0 +1,15 @@
+﻿int count=56;
+string fullName="Ravi Tambade";
+bool status=false;
+int marks=78;
+float amount=56.67f;
+
+if(count <100){
+Console.WriteLine("Value is less than 100");
+}
+else{
+Console.WriteLine("Value is greater than or equal to 100");
+}
+count ++;
+Console.WriteLine("Count ={0}", count);
+Console.WriteLine("Hello, World!");
