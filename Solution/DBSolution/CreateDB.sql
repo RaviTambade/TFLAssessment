8e560853c1364b507a76c55bba05a22a71638a07
Ravi Tambade <ravi.tambade@transflower.in>
2024-01-30 16:45:08 +0530

    Assessment Crafting

--- Solution/DBSolution/CreateDB.sql
+++ Solution/DBSolution/CreateDB.sql
@@ -1,2 +1,57 @@
  --Database Creation
 
+-- Table structure of Assessment
+
+--
+/*Tests
+testid
+testdate
+duration
+designedby
+approvedby
+
+
+Testpapers
+Testpaperid
+skillid
+
+Questions
+Qestionid
+correctAnswerid
+
+
+Answers
+Answerid
+Title
+
+TestResult
+TestresultId
+TestId
+empid
+
+
+
+Skills:
+	skillId
+	Title
+		1	java
+		2	CSharp
+		3	mysql
+		4	python
+
+SkillSetRating:
+	skillratingid
+	empid
+	skillid
+	rating
+		1	23	1	7
+		2	23	2	8
+		3	21	1	8
+		
+Employees
+	empid
+	firstname
+	lastname
+	contactnumber
+	emailid
+*/
