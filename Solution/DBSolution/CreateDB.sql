01aed27daf04ec657744a5d682e6f42129659dc6
Ravi Tambade <ravi.tambade@transflower.in>
2024-02-07 15:46:47 +0530

    database design

--- Solution/DBSolution/CreateDB.sql
+++ Solution/DBSolution/CreateDB.sql
@@ -1,62 +1,19 @@
-
-
---
-/*Tests
-testid
-testdate
-duration
-designedby
-approvedby
-
-
-Testpapers
-Testpaperid
-skillid
-
-Questions
-Qestionid
-correctAnswerid
-
-
-Answers
-Answerid
-Title
-
-TestResult
-TestresultId
-TestId
-empid
-
-
-
-Skills:
-	skillId
-	Title
-		1	java
-		2	CSharp
-		3	mysql
-		4	python
-
-SkillSetRating:
-	skillratingid
-	empid
-	skillid
-	rating
-		1	23	1	7
-		2	23	2	8
-		3	21	1	8
-		
-Employees
-	empid
-	firstname
-	lastname
-	contactnumber
-	emailid
-*/
+ 
 
 CREATE DATABASE assesmentdb;
 
 use assesmentdb; 
+
+---
+
+
+
+
+
+
+
+----
+
 
 CREATE TABLE
     employee(
@@ -113,30 +70,3 @@ where employee.id=SkillSetRating.employeeid and Skill.Title="Java";
 
 CONSTRAINT EmployeeID FOREIGN KEY(EmployeeID) REFERENCES employee(id) 
 --Condition Query:
-
-------------------------------------------------------------------------------------------------------------
-*Evaulator*-> Portal -> Login -> Online Exam -> Technology ->FrontEnd ->(Exam Conduction)javascript,React,Angular
-                               ->Fresher Criteria                     ->BackEnd  ->Spring boot,Hybernet,.NETWORK 
-                                                                      ->Full Stack-> All
-		       Rating->Technical
-		             ->Communication
-			         ->Verdict
-			         ->Goals,Approch to Client,Learning,Leadership,Work ethics,team work,innovation
-					 
-*Candidate* -> Portal -> Login -> Technical Round -> Feedback
-                    -> Internal
-					-> External
----------------------------------------------------------------------------------------------------------------					
-*Required Tables*
-Evaulator (EvaulatorId,Emp.Name,Emp.Designation,EmpIdFk,SubjectIdFk)
-Candidate (CandidateId,Emp.Name,Emp.Designation,Emp.JoinDate,Experince,EmpId)
-
-Javscript (QustionId,Qustion,Option 1,Option2,Option3,Option4,Correct Answer)
-React     (QustionId,Qustion,Option 1,Option2,Option3,Option4,Correct Answer)
-Angular   (QustionId,Qustion,Option 1,Option2,Option3,Option4,Correct Answer)
-DotNet    (QustionId,Qustion,Option 1,Option2,Option3,Option4,Correct Answer)
-Qustion Bank (QBankId,QustionIdFk,SelectedAnswer)
-Candidate Performance (CPId,Marks,QBankIdFk,QustionIdFk)
-
-
-
