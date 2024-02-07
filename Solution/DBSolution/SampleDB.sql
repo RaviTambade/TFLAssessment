af42c54f1e961aa7dd54211c4fe492aabe53a4a8
Bhupebdra5454 <akshayw2512@gmail.com>
2024-02-07 17:15:25 +0530

    AssesmentDB Tables

--- Solution/DBSolution/SampleDB.sql
+++ Solution/DBSolution/SampleDB.sql
@@ -1,2 +1,23 @@
- --Database Creation
+SAMPLE DATA
 
+INSERT INTO TechnicalSkills(Title) VALUES(CORE JAVA);
+INSERT INTO TechnicalSkills(Title) VALUES(ADV JAVA);
+INSERT INTO TechnicalSkills(Title) VALUES(DOTNET);
+INSERT INTO TechnicalSkills(Title) VALUES(MICRO SERVICES);
+INSERT INTO TechnicalSkills(Title) VALUES(REACT);
+INSERT INTO TechnicalSkills(Title) VALUES(ANGULAR);
+INSERT INTO TechnicalSkills(Title) VALUES(C#);
+INSERT INTO TechnicalSkills(Title) VALUES(JavaScriept);
+
+INSERT INTO SubjectExperties(EmployeeId,TechnicalSkillId,CertifiedOn)VALUES(1,1,'2023-02-15');
+INSERT INTO SubjectExperties(EmployeeId,TechnicalSkillId,CertifiedOn)VALUES(2,2,'2023-02-15');
+INSERT INTO SubjectExperties(EmployeeId,TechnicalSkillId,CertifiedOn)VALUES(3,5,'2023-02-15');
+INSERT INTO SubjectExperties(EmployeeId,TechnicalSkillId,CertifiedOn)VALUES(4,6,'2023-02-15');
+
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(OOPS,1);
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(Multithreading,1);
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(Loops,1);
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(HyberNet,2);
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(Spring Boot,2);
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(JSON,5);
+INSERT INTO EvaluationCriterias(Title,SkillId) VALUES(Components,6);
