f29f247de52ccafefff0d0e1ad258d7cd792ca1e
Ravi Tambade <ravi.tambade@transflower.in>
2024-02-09 15:46:54 +0530

    Business Queries

--- Solution/DBSolution/QueryDB.sql
+++ Solution/DBSolution/QueryDB.sql
@@ -1,2 +1,27 @@
  --Database Creation
 
+use assessmentdb;
+
+select * from technicalskills;
+
+-- Select all questions  beong to ADV JAVA
+select * from questions where skillid=(select  techskid from technicalskills where title ="ADV JAVA");
+
+select * from employee;
+
+select  subexid from subjectexperties where technicalskillid=(select  techskid from technicalskills where title ="ADV JAVA");
+
+-- Show all employee details who are experts in CORE JAVA
+select * from employee  where employee.id in(
+								select  employeeid from subjectexperties employee
+								where technicalskillid=( select  techskid from technicalskills 
+														 where title ="CORE JAVA")
+								);
+
+-- Show question titles of test conducted on specific DATE
+
+
+
+
+
+
