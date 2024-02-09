ac0df0de2b78bf5c81d50a968c105b176a220096
shri <patilshridhar8056@gmail.com>
2024-02-09 15:12:58 +0530

    dbquery

--- Solution/DBSolution/CreateDB.sql
+++ Solution/DBSolution/CreateDB.sql
@@ -1,7 +1,6 @@
 create database assessmentdb;
 use assessmentdb;
 
-
 CREATE TABLE employee(
       id INT AUTO_INCREMENT PRIMARY KEY,
       fname VARCHAR(20) NOT NULL,
@@ -9,15 +8,13 @@ CREATE TABLE employee(
       email VARCHAR(50) NOT NULL,
       contactno VARCHAR(10) NOT NULL
 );
+select * from employee;
 
-
-CREATE TABLE
-  technicalskills(
+CREATE TABLE technicalskills(
   techskid INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(20)
 );
-
-
+select * from technicalskills;
 
 CREATE TABLE subjectexperties(
    subexid INT PRIMARY KEY AUTO_INCREMENT,
@@ -27,9 +24,7 @@ CREATE TABLE subjectexperties(
    CONSTRAINT fk_employee_subjectexperties_employeeid FOREIGN KEY(employeeid) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE CASCADE,
    CONSTRAINT fk_technicalskills_subjectexperties_technicalskillid FOREIGN KEY(technicalskillid) REFERENCES technicalskills(techskid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-
-
+select * from subjectexperties;
 
 CREATE TABLE evaluationcriterias(
    evacriid INT PRIMARY KEY AUTO_INCREMENT,
@@ -37,7 +32,7 @@ CREATE TABLE evaluationcriterias(
    skillid INT,
    CONSTRAINT fk_technicalskills_evaluationcriterias_skillid FOREIGN KEY(skillid) REFERENCES technicalskills(techskid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
+select * from evaluationcriterias;
 
 create table questions (
      qid INT PRIMARY KEY AUTO_INCREMENT,
@@ -52,11 +47,7 @@ create table questions (
 	 CONSTRAINT fk_technicalskills_qestions_skillid FOREIGN KEY(skillid) REFERENCES technicalskills(techskid) ON UPDATE CASCADE ON DELETE CASCADE,
      CONSTRAINT fk_evaluationcriterias_qestions_evacrid FOREIGN KEY(evacriid) REFERENCES evaluationcriterias(evacriid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-
-
-
-
+select * from questions;
 
 create table tests (
 id INT PRIMARY KEY AUTO_INCREMENT,
@@ -66,18 +57,19 @@ subexid INT ,
 createdon DATE,
 modifiedon DATE,
 sheduledon DATE,
- CONSTRAINT fk_technicalskills_tests_skillid FOREIGN KEY(skillid) REFERENCES technicalskills(techskid) ON UPDATE CASCADE ON DELETE CASCADE,
-     CONSTRAINT fk_subjectexperties_tests_subexid FOREIGN KEY(subexid) REFERENCES subjectexperties(subexid) ON UPDATE CASCADE ON DELETE CASCADE
+CONSTRAINT fk_technicalskills_tests_skillid FOREIGN KEY(skillid) REFERENCES technicalskills(techskid) ON UPDATE CASCADE ON DELETE CASCADE,
+CONSTRAINT fk_subjectexperties_tests_subexid FOREIGN KEY(subexid) REFERENCES subjectexperties(subexid) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
+select * from tests;
 
 create table testasscriteria(
-id INT,
+id INT PRIMARY KEY AUTO_INCREMENT,
 testid INT,
 evaluationcriteriaid INT ,
 CONSTRAINT fk_tests_testasscriteria_testid FOREIGN KEY(testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT fk_evaluationcriterias_testasscriteria_evaluationcriteriaid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(evacriid) ON UPDATE CASCADE ON DELETE CASCADE
 );
+select * from testasscriteria;
 
 create table testquestions(
 testquestionid INT  PRIMARY KEY AUTO_INCREMENT,
@@ -86,26 +78,25 @@ questionid INT,
 CONSTRAINT fk_tests_testquestions_testid FOREIGN KEY(testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE,
 CONSTRAINT fk_questions_testquestions_questionid FOREIGN KEY(questionid) REFERENCES questions(qid) ON UPDATE CASCADE ON DELETE CASCADE
 );
+select * from testquestions;
 
 create table candidateanswers(
-	questionsanswereid INT,
+	questionsanswereid INT PRIMARY KEY AUTO_INCREMENT,
 	employeeid INT,
 	testquestionid INT,
  	answerkey VARCHAR(100),
 	CONSTRAINT fk_employee_candidateanswers_employeeid FOREIGN KEY(employeeid) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT fk_testquestions_candidateanswers_testquestionsid FOREIGN KEY(testquestionid) REFERENCES testquestions (testquestionid) ON UPDATE CASCADE ON DELETE CASCADE
-	
 );
-
+select * from candidateanswers;
 
 create table candidatetestresults(
-	candidateresultid INT,
+	candidateresultid INT PRIMARY KEY AUTO_INCREMENT,
 	testid INT,
 	marks INT,
  	CONSTRAINT fk_test_candidatetestresults FOREIGN KEY (testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
-
-
+select * from candidatetestresults;
 
 Create table interviews(
 	interviewid INT PRIMARY KEY AUTO_INCREMENT,
@@ -115,6 +106,7 @@ Create table interviews(
 	CONSTRAINT fk_subjectexperties_interviews_subexid FOREIGN KEY(subexid) REFERENCES subjectexperties(subexid) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT fk_employee_interviews_employeeid FOREIGN KEY(employeeid) REFERENCES employee(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
+select * from interviews;
  
 
 Create table interviewcriterias(
@@ -124,9 +116,10 @@ Create table interviewcriterias(
 	CONSTRAINT fk_interviews_interviewcriterias_interviewid FOREIGN KEY(interviewid) REFERENCES interviews(interviewid) ON UPDATE CASCADE ON DELETE CASCADE,
 	CONSTRAINT fk_evaluationcriterias_interviewcriterias_evacriid FOREIGN KEY(evacriid) REFERENCES evaluationcriterias(evacriid) ON UPDATE CASCADE ON DELETE CASCADE
 );
+-- techintacid - techical interview assessment criteria Id
+
+select * from interviewcriterias;
     
-
-
 Create table interviewresults(
 	resultid INT PRIMARY KEY AUTO_INCREMENT,
 	techintacid INT,
@@ -134,4 +127,4 @@ Create table interviewresults(
 	comments VARCHAR(200),
 	CONSTRAINT fk_interviewcriterias_interviewresults_techintacid FOREIGN KEY(techintacid) REFERENCES interviewcriterias(techintacid) ON UPDATE CASCADE ON DELETE CASCADE
 	);
-    
+select * from interviewresults;    
--- Solution/DBSolution/SampleDB.sql
+++ Solution/DBSolution/SampleDB.sql
@@ -7,6 +7,7 @@ insert into employee(fname, lname, email, contactno)values("Vaibhav","Walekar","
 insert into employee(fname, lname, email, contactno)values("Shree","pawer","spawar@gmail.com","8654653254");
 insert into employee(fname, lname, email, contactno)values("Aniket","Zarekar","Zarekar@gmail.com","9764263254");
 
+select * from employee;
 
 
 INSERT INTO technicalskills(title) VALUES('CORE JAVA');
@@ -18,7 +19,7 @@ INSERT INTO technicalskills(title) VALUES('ANGULAR');
 INSERT INTO technicalskills(title) VALUES('C#');
 INSERT INTO technicalskills(title) VALUES('JavaScriept');
 
-
+select * from technicalskills;
 
 
 INSERT INTO subjectexperties(employeeid,technicalskillid,certifiedon)VALUES(1,1,'2023-02-15');
@@ -30,8 +31,7 @@ INSERT INTO subjectexperties(employeeid,technicalskillid,certifiedon)VALUES(2,5,
 INSERT INTO subjectexperties(employeeid,technicalskillid,certifiedon)VALUES(3,7,'2023-01-25');
 INSERT INTO subjectexperties(employeeid,technicalskillid,certifiedon)VALUES(4,2,'2023-02-05');
 
-
-
+select * from subjectexperties;
 
 INSERT INTO evaluationcriterias(title,skillid) VALUES('OOPS',1);
 INSERT INTO evaluationcriterias(title,skillid) VALUES('Multithreading',1);
@@ -44,44 +44,129 @@ INSERT INTO evaluationcriterias(title,skillid) VALUES('OOPS',7);
 INSERT INTO evaluationcriterias(title,skillid) VALUES('Multithreading',7);
 INSERT INTO evaluationcriterias(title,skillid) VALUES('Loops',7);
 
+select * from evaluationcriterias;
+
 INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Number of primitive data types in Java are?','6','7','8','9','8','1');
 INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Which of the following language was developed as the first purely object programming language?','SmallTalk','C++','Kotlin','Java','SmallTalk','1');
-INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Who developed object-oriented programming?','Adele Goldberg','Dennis Ritchie','Alan Kay','
-Andrea Ferro','Alan Kay','1');
-INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Which of the following is not an OOPS concept?','Exception','Abstraction','Polymorphism','
-Encapsulation','Exception','1');
-
-
-INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Which of the following language supports polymorphism but not the classes?
-','C++ programming language
-','Java programming language','Ada programming language','
-C# programming language','Ada programming language','1');
-
+INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Who developed object-oriented programming?','Adele Goldberg','Dennis Ritchie','Alan Kay','Andrea Ferro','Alan Kay','1');
+INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Which of the following is not an OOPS concept?','Exception','Abstraction','Polymorphism','Encapsulation','Exception','1');
+INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(1,'Which of the following language supports polymorphism but not the classes?','C++ programming language','Java programming language','Ada programming language','C# programming language','Ada programming language','1');
 INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'Spring Boot is used for developing?','Web applications','Distributed applications (Restful web services)','Microservices','All of the above','All of the above','5');
-
 INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'How can a Spring Boot application be packaged and distributed?','As a JAR file ','As a WAR file','As a ZIP file','All of the above.','All of the above.','5');
-
-
-INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'What AOP stands for?','Aspect Oriented Programming','Any Object Programming','Asset Oriented Programming','
-Asset Oriented Protocol','Aspect Oriented Programming','5');
-
+INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'What AOP stands for?','Aspect Oriented Programming','Any Object Programming','Asset Oriented Programming','Asset Oriented Protocol','Aspect Oriented Programming','5');
 INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'Which of the following is the correct name of React.js?',' React','React.js', 'ReactJs','All of the above','All of the above','5');
+INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'How to use ref keyword in beans.xml?','Using setter method only.','Using constructor argument only.','Using setter method and constructor argument both.','None of the above.','Using setter method and constructor argument both.','5');
 
-
-INSERT INTO questions(skillid, question, a, b, c, d, answerkey, evacriid) VALUES(2,'How to use ref keyword in beans.xml?','Using setter method only.','Using constructor argument only.','Using setter method and constructor argument both.','
-None of the above.','Using setter method and constructor argument both.','5');
-
-
+select * from questions;
 
 
 INSERT INTO tests(skillid,duration,subexid,createdon,modifiedon,sheduledon) VALUES (1,"00:30:00",1,'2023-12-05','2024-02-05','2024-02-07');
 INSERT INTO tests(skillid,duration,subexid,createdon,modifiedon,sheduledon) VALUES (1,"00:40:00",1,'2023-11-05','2024-01-05','2024-02-07');
 INSERT INTO tests(skillid,duration,subexid,createdon,modifiedon,sheduledon) VALUES (1,"00:20:00",1,'2023-09-05','2024-01-05','2024-02-08');
+INSERT INTO tests(skillid,duration,subexid,createdon,modifiedon,sheduledon) VALUES (2,"00:30:00",2,'2023-09-21','2023-12-05','2024-01-05');
+INSERT INTO tests(skillid,duration,subexid,createdon,modifiedon,sheduledon) VALUES (3,"01:00:00",1,'2023-11-05','2023-12-21','2024-01-24');
+INSERT INTO tests(skillid,duration,subexid,createdon,modifiedon,sheduledon) VALUES (4,"01:30:00",2,'2023-09-05','2023-10-10','2024-01-18');
+
+select * from tests;
 
 
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(1,4);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(1,2);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(2,2);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(2,4);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(1,3);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(5,4);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(2,3);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(1,5);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(3,1);
+INSERT INTO testasscriteria(testid, evaluationcriteriaid)VALUES(4,5);
+
+select * from testasscriteria;
 
 
+INSERT INTO testquestions(testid, questionid)VALUES(1,4);
+INSERT INTO testquestions(testid, questionid)VALUES(2,1);
+INSERT INTO testquestions(testid, questionid)VALUES(3,2);
+INSERT INTO testquestions(testid, questionid)VALUES(1,3);
+INSERT INTO testquestions(testid, questionid)VALUES(1,2);
+INSERT INTO testquestions(testid, questionid)VALUES(1,5);
+INSERT INTO testquestions(testid, questionid)VALUES(2,4);
+INSERT INTO testquestions(testid, questionid)VALUES(3,4);
+INSERT INTO testquestions(testid, questionid)VALUES(3,3);
+INSERT INTO testquestions(testid, questionid)VALUES(4,2);
 
+select * from testquestions;
+
+
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(1,4,'c');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(2,4,'a');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(1,1,'c');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(2,1,'d');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(1,2,'c');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(2,1,'a');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(4,2,'b');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(5,3,'c');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(1,2,'b');
+INSERT INTO candidateanswers(employeeid, testquestionid,answerkey)VALUES(2,3,'c');
+
+select * from candidateanswers;
+
+
+INSERT INTO candidatetestresults(testid, marks)VALUES(2,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(1,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(3,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(4,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(5,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(6,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(1,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(2,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(3,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(4,1);
+INSERT INTO candidatetestresults(testid, marks)VALUES(1,1);
+
+select * from candidatetestresults;
+
+
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-02-10',1,2);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-02-10',2,3);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-03-12',1,4);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-03-01',3,5);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-02-15',1,6);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-03-10',4,2);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-02-20',1,3);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-02-12',2,4);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-03-11',1,5);
+INSERT INTO interviews(dateofinterview, subexid, employeeid)VALUES('2024-03-15',3,6);
+
+select * from interviews;
+
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(1,6);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(1,5);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(2,3);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(1,2);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(3,4);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(2,4);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(3,6);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(2,5);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(1,7);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(2,1);
+INSERT INTO interviewcriterias(interviewid, evacriid)VALUES(1,8);
+
+select * from interviewcriterias;
+
+
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(1,8,"good");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(2,6,"avarage");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(1,9,"very good");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(2,7,"nice");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(3,9,"very good");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(4,8,"good");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(5,10,"very good");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(6,7,"good");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(7,8,"nice");
+INSERT INTO interviewresults(techintacid, rating, comments)VALUES(8,8,"good");
+
+select * from interviewresults;
 
 
 
