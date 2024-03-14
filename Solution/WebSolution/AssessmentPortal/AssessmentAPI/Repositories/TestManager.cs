c5e3ba2e06b46594464c6d6e2348118f6bd52de6
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2024-03-14 22:48:38 +0530

    new changes done

--- Solution/WebSolution/AssessmentPortal/AssessmentAPI/Repositories/TestManager.cs
+++ Solution/WebSolution/AssessmentPortal/AssessmentAPI/Repositories/TestManager.cs
@@ -97,40 +97,5 @@ public int GetCandidateScore(int candidateId,int testId ){
 }
 
 
-
-
-// public bool  CandidateAnswers(List<CandidateAnswer> answers)
-// {
-//     bool status=false;
-//     string connectionString="server=localhost;port=3306;user=root;password=password;database=assessmentdb";
-//     MySqlConnection connection = new MySqlConnection(connectionString);
-//      try{
-//        connection.Open();
-
-//             foreach (var answer in answers)
-//             {
-//                 string query = "INSERT INTO candidateanswers (employeeid, testquestionid, answerkey) VALUES (@employeeId, @testQuestionId, @answerKey)";
-//                 MySqlCommand command = new MySqlCommand(query, connection);
-//                 command.Parameters.AddWithValue("@employeeId", candidateId);
-//                 command.Parameters.AddWithValue("@testQuestionId", answer.TestQuestionId);
-//                 command.Parameters.AddWithValue("@answerKey", answer.Answer);
-
-//                 int rowsAffected = command.ExecuteNonQuery();
-//                 if (rowsAffected > 0)
-//                 {
-//                     status = true;
-//                 }
-//             }  
-//     }
-//     catch(Exception e){
-//        Console.WriteLine(e.Message);
-//     }
-//     finally{
-//         connection.Close();
-//     }
-//     return status;
-// }
-
-
 }
 
