1d114ebb61df9e7ea0e8f05d0965ba8493363be6
Ravi Tambade <ravi.tambade@transflower.in>
2024-03-16 11:26:59 +0530

    test

--- Solution/WebSolution/AssessmentPortal/HTML/TRYJQuery/TestOnline/app.js
+++ Solution/WebSolution/AssessmentPortal/HTML/TRYJQuery/TestOnline/app.js
@@ -1,15 +1,18 @@
  
 //global variable 
 var current = 0;
+
+var subjects = [];
 var questions = [];
+var time={};
+
 var intervalId ;
 var remainingTime=60;
 var score;
-var time={};
+
 var testId=1;
 var candidateId = 3;
-var subjects = [];
-var questions = [];
+
 
 
 //rest api urls
@@ -18,45 +21,107 @@ var testsapi= "http://localhost:5238/tests/";
 var starttimeurl = "http://localhost:5238/test/setstarttime";
 var candidateanswersurl = "http://localhost:5238/answersheet/candidates/";
 var endtimeurl = "http://localhost:5238/test/setendtime";
-var resulturl = "http://localhost:5238/result/candidates/"
+var resulturl = "http://localhost:5238/result/candidates/";
 
 
 $(document).ready(function () { 
 
+  //Test start and submit handlers
+  $("#btnStart").click(()=>{
+      activateNavigation(false);
+      let currentIndex=0;
+      startTime=getCurrentDateTime();
+      let questionsAPI = testsapi + testId;
+      $.get(questionsAPI, function (data, status) {
+        questions = (data);
+        questions.map((question) => { question["answer"] = "No" });
+        showQuestion(currentIndex);  
+      });
+      
+        //let url = "http://localhost:5238/test/setstarttime";
+        var testStartTime={"CandidateId":candidateId,"TestId":testId,"Time":startTime};
+        $.ajax({     
+              url: starttimeurl,
+              type: 'POST',
+              contentType: 'application/json',
+              data: JSON.stringify(testStartTime),
     
-$("#btnStart").click(()=>{
-    activateNavigation(false);
-    let currentIndex=0;
-    startTime=getCurrentDateTime();
-    let questionsAPI = testsapi + testId;
-    $.get(questionsAPI, function (data, status) {
-       questions = (data);
-       questions.map((question) => { question["answer"] = "No" });
-       showQuestion(currentIndex);  
-    });
-     
-      //let url = "http://localhost:5238/test/setstarttime";
-      var testStartTime={"CandidateId":candidateId,"TestId":testId,"Time":startTime};
-      $.ajax({     
-            url: starttimeurl,
+              success: function (response) {
+                console.log("inside post "+response);
+                console.log(""+response);
+              },
+              error: function (xhr, status, error) {
+                console.error(xhr.responseText);
+                console.log("inside post "+error);
+              }
+      });
+        activateNavigation(false);
+        testTimeLimit();
+    })
+  
+  $("#btnSubmit").click( ()=> {
+    clearInterval(intervalId);
+    $("#btnStart").prop("disabled", true);
+    $("#btnSubmit").prop("disabled", true);
+    activateNavigation(true);
+    var finalCandidateAnswers = [];
+  
+    questions.map((question) => {
+      let questionId = question.id;
+      let answer = question.answer;
+      
+      finalCandidateAnswers.push({ "Answer": answer, "TestQuestionId": questionId });
+    })
+    var endTime=getCurrentDateTime();
+
+    
+    let evaluateUrl = candidateanswersurl+candidateId+"/tests/"+testId;
+    $.ajax({
+            url: evaluateUrl,
             type: 'POST',
             contentType: 'application/json',
-            data: JSON.stringify(testStartTime),
+            data: JSON.stringify(finalCandidateAnswers),
   
             success: function (response) {
-              console.log("inside post "+response);
-              console.log(""+response);
+              // Show result
+              console.log(response);
             },
             error: function (xhr, status, error) {
               console.error(xhr.responseText);
-              console.log("inside post "+error);
             }
     });
-      activateNavigation(false);
-      testTimeLimit();
-  })
+    
+    var testEndTime ={"CandidateId":candidateId,"TestId":testId,"Time":endTime}
   
+    $.ajax({
+            url: endtimeurl,
+            type: 'PUT',
+            contentType: 'application/json',
+            data: JSON.stringify(testEndTime),
+  
+            success: function (response) {
+              // Show result
+              console.log(response);
+            },
+            error: function (xhr, status, error) {
+              console.error(xhr.responseText);
+            }
+    });   
+  }); 
+  
+  //Show Result
+  $("#btnResult").click(()=>{
+    
+    //result/candidates/{candidateId}/test/{testId}/
+    let url = resulturl+candidateId+"/test/"+testId;
+    $.get(url, function (data, status) {
+      score = (data);
+      $("#lblresult").text("Your Score is:"+score);
+    });
+  })
+        
 
+ //refersh progress bar on answer  selection
   $("input[type='radio']").click(()=> {
     var selectedOption = $("input[name='answer']:checked").attr("id");
     if (selectedOption) {
@@ -64,6 +129,8 @@ $("#btnStart").click(()=>{
     }
     loadProgressBar();
   });
+
+  //Navigation handlers
   
   $("#btnFirst").click(()=> {
     current = 0;
@@ -148,67 +215,5 @@ $("#btnStart").click(()=>{
     else {
       $("[name='answer']").prop("checked", false);
     }
-  });
-  
-
-  $("#btnSubmit").click( ()=> {
-    clearInterval(intervalId);
-    $("#btnStart").prop("disabled", true);
-    $("#btnSubmit").prop("disabled", true);
-    activateNavigation(true);
-    var finalCandidateAnswers = [];
-  
-    questions.map((question) => {
-      let questionId = question.id;
-      let answer = question.answer;
-       
-      finalCandidateAnswers.push({ "Answer": answer, "TestQuestionId": questionId });
-    })
-    var endTime=getCurrentDateTime();
-
-    
-    let evaluateUrl = candidateanswersurl+candidateId+"/tests/"+testId;
-    $.ajax({
-            url: evaluateUrl,
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(finalCandidateAnswers),
-  
-            success: function (response) {
-              // Show result
-              console.log(response);
-            },
-            error: function (xhr, status, error) {
-              console.error(xhr.responseText);
-            }
-    });
-     
-    var testEndTime ={"CandidateId":candidateId,"TestId":testId,"Time":endTime}
-  
-    $.ajax({
-            url: endtimeurl,
-            type: 'PUT',
-            contentType: 'application/json',
-            data: JSON.stringify(testEndTime),
-  
-            success: function (response) {
-              // Show result
-              console.log(response);
-            },
-            error: function (xhr, status, error) {
-              console.error(xhr.responseText);
-            }
-    });   
-  }); 
-  
-  $("#btnResult").click(()=>{
-    
-    //result/candidates/{candidateId}/test/{testId}/
-    let url = resulturl+candidateId+"/test/"+testId;
-    $.get(url, function (data, status) {
-      score = (data);
-      $("#lblresult").text("Your Score is:"+score);
-    });
-  })
-                                                               
+  });                                                    
 }) 
