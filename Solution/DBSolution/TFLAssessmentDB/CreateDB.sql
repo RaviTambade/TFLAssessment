31afec6cf62bd57d0936f6a31265f7508115c2ae
Ravi Tambade <ravi.tambade@transflower.in>
2024-03-19 15:36:20 +0530

    database foreignkey constraints updation

--- Solution/DBSolution/TFLAssessmentDB/CreateDB.sql
+++ Solution/DBSolution/TFLAssessmentDB/CreateDB.sql
@@ -1,3 +1,4 @@
+-- Active: 170712create3530557@@127.0.0.1@3306@assessmentdb
 -- Assessment Database for Online Examination Transflower
 
 -- Active: 1707123530557@@127.0.0.1@3306@assessmentdb
@@ -23,15 +24,15 @@ CREATE TABLE subjectmatterexperts(
 	employeeid INT,
 	subjectid INT,
 	certidicationdate DATE,
-	CONSTRAINT fk_subjectmatterexperts_employeeid FOREIGN KEY(employeeid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_subjectmatterexperts_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_sme_employees_employeeid FOREIGN KEY(employeeid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_sme_subjects_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE evaluationcriterias(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	title VARCHAR(20),
 	subjectid INT,
-	CONSTRAINT fk_evaluationcriterias_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_eval_subjects_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 create table questionbank (
@@ -44,8 +45,8 @@ create table questionbank (
 	d VARCHAR(100),
 	answerkey CHAR,
 	evaluationcriteriaid INT,
-	CONSTRAINT fk_subjects_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_evaluationcriterias_evaluationcriteriaid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_qbank_subjects_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_qbank_eval_evalid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -58,8 +59,8 @@ create table tests (
 	modificationdate DATETIME,
 	scheduleddate DATETIME,
 	status ENUM("created","scheduled", "cancelled","conducted")  DEFAULT "created",
-	CONSTRAINT fk_subjects_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_subjectmatterexperts_smeid FOREIGN KEY(smeid) REFERENCES subjectmatterexperts(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_tests_subjects_subjectid FOREIGN KEY(subjectid) REFERENCES subjects(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_tests_sme_smeid FOREIGN KEY(smeid) REFERENCES subjectmatterexperts(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -67,8 +68,8 @@ create table testassessmentcriterias(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	testid INT,
 	evaluationcriteriaid INT ,
-	CONSTRAINT fk_tests_testid FOREIGN KEY(testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_evaluationcriteriaid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_as_tests_testid FOREIGN KEY(testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_as_evalcriteria_evalcaritid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -77,7 +78,7 @@ create table testquestions(
 	testid INT,
 	questionbankid INT,
 	CONSTRAINT fk_tests_testid FOREIGN KEY(testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_questionbank_questionbankid FOREIGN KEY(questionbankid) REFERENCES questionbank(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_testqt_qbank_questionbankid FOREIGN KEY(questionbankid) REFERENCES questionbank(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 create table candidateanswers(
@@ -85,8 +86,8 @@ create table candidateanswers(
 	candidateid INT,
 	testquestionid INT,
  	answerkey CHAR,
-	CONSTRAINT fk_employees_candidateid FOREIGN KEY(candidateid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_testquestions_testquestionid FOREIGN KEY(testquestionid) REFERENCES testquestions(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_emp_candidateid FOREIGN KEY(candidateid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_tstqt_testquestionid FOREIGN KEY(testquestionid) REFERENCES testquestions(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 create table candidatetestresults(
@@ -96,8 +97,8 @@ create table candidatetestresults(
 	teststarttime DATETIME,
 	testendtime DATETIME,
 	candidateid INT,
-	CONSTRAINT fk_employees_candidateId FOREIGN KEY (candidateId) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT fk_tests_testid FOREIGN KEY (testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_employees_candid FOREIGN KEY (candidateId) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT fk_test_testid FOREIGN KEY (testid) REFERENCES tests(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
@@ -106,16 +107,16 @@ Create table interviews(
 	interviewdate DATE,
 	smeid INT,
 	candidateid INT,
-	CONSTRAINT fk_subjectmatterexperts_smeid FOREIGN KEY(smeid) REFERENCES subjectmatterexperts(id) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_employees_candidateid FOREIGN KEY(candidateid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_sme_smeid FOREIGN KEY(smeid) REFERENCES subjectmatterexperts(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_emp_candtid FOREIGN KEY(candidateid) REFERENCES employees(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 Create table interviewcriterias(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	interviewid INT,
 	evaluationcriteriaid INT,
-	CONSTRAINT fk_interviews_interviewid FOREIGN KEY(interviewid) REFERENCES interviews(interviewid) ON UPDATE CASCADE ON DELETE CASCADE,
-	CONSTRAINT fk_evaluationcriterias_evaluationcriteriaid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_interviews_interviewid FOREIGN KEY(interviewid) REFERENCES interviews(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT fk_evalriterias_evalcriteaid FOREIGN KEY(evaluationcriteriaid) REFERENCES evaluationcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 Create table interviewresults(
@@ -123,5 +124,5 @@ Create table interviewresults(
 	interviewcriteriaid INT,
 	ratings INT,
 	comments VARCHAR(200),
-	CONSTRAINT fk_interviewresults_interviewcriterias_interviewcriteriaid FOREIGN KEY(interviewcriteriaid) REFERENCES interviewcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT fk_intresults_intcrite_intcriteid FOREIGN KEY(interviewcriteriaid) REFERENCES interviewcriterias(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
