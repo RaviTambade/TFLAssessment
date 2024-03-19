62a6af73cd71a4c4e5c7dd0a8ede7fdd96348a4c
Ravi Tambade <ravi.tambade@transflower.in>
2024-03-19 19:51:21 +0530

    ProductCatalog unit test

---
+++ Solution/TestSolution/ProductCatalog/ProductService.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+namespace ProductCatalog;
+public class ProductService
+{
+    private List<Product> _products = new List<Product>();
+    private int _nextId = 1;
+
+    public Product CreateProduct(string name, double price)
+    {
+        var product = new Product { Id = _nextId++, Name = name, Price = price };
+        _products.Add(product);
+        return product;
+    }
+
+    public Product GetProductById(int id)
+    {
+        return _products.Find(p => p.Id == id);
+    }
+
+    public void UpdateProduct(int id, string newName, double newPrice)
+    {
+        var product = GetProductById(id);
+        if (product != null)
+        {
+            product.Name = newName;
+            product.Price = newPrice;
+        }
+        else
+        {
+            throw new ArgumentException("Product not found");
+        }
+    }
+
+    public void DeleteProduct(int id)
+    {
+        var product = GetProductById(id);
+        if (product != null)
+        {
+            _products.Remove(product);
+        }
+        else
+        {
+            throw new ArgumentException("Product not found");
+        }
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return _products;
+    }
+}
