64c4cc33da3e6f00aaa611da5789e830a721b523
Akashajab1999 <118430799+Akashajab1999@users.noreply.github.com>
2024-03-22 22:48:49 +0530

    restructuring

--- Solution/WebSolution/AssessmentPortal/AssessmentAPI/Requests/CandidateTestTimeRequest.cs
+++
@@ -1,9 +0,0 @@
-using Assessment.Entities;
-
-namespace Requests;
-public class CandidateTestTimeRequest{
-
-    public int CandidateId{get;set;}
-    public int TestId{get;set;}
-    public TestTime Time{get;set;}
-}
--- Solution/WebSolution/AssessmentPortal/AssessmentAPI/Responses/QuestionTitleResponse.cs
+++
@@ -1,5 +0,0 @@
-namespace Entities;
-public class QuestionTitleResponse{
-    public int Id{get;set;}
-    public string Title{get;set;}
-}
