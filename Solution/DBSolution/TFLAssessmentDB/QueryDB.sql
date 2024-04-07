ecc23d4df2ec8c08f7f84aa2d13f30f23f71b394
PragatiBangar <bangarpragati11@gmail.com>
2024-04-07 22:35:58 +0530

    querydb

--- Solution/DBSolution/TFLAssessmentDB/QueryDB.sql
+++ Solution/DBSolution/TFLAssessmentDB/QueryDB.sql
@@ -5,8 +5,8 @@ use assessmentdb;
 select * from interviews;
 select * from interviewcriterias;
 select * from interviewresults;
- select questionbank.id,questionbank.title,questionbank.a,questionbank.b,questionbank.c,questionbank.d from questionbank inner join subjects on subjects.id=questionbank.subjectid
- where subjects.title="ADVJAVA";
+select questionbank.id,questionbank.title,questionbank.a,questionbank.b,questionbank.c,questionbank.d from questionbank inner join subjects on subjects.id=questionbank.subjectid
+where subjects.title="ADVJAVA";
 select * from test;
 -- Select all questions  beong to ADV JAVA
 select * from questionbank where subjectid=(select id from subjects where title ="ADVJAVA");
@@ -15,16 +15,15 @@ select * from questionbank inner join testquestions on testquestions.questionban
 
 INSERT INTO candidateanswers (candidateid, testquestionid, answerkey) VALUES (@candidateId, @testQuestionId, @answerKey);
 
-
 update candidatetestresults set testendtime =@testendtime where candidateid=@candidateid and testid=@testid;
 update candidatetestresults set testendtime ="2015-11-05 14:35:00" where candidateid=2 and testid=1;
 
 
 
 select questionbank.id, questionbank.title, subjects.title as subject ,evaluationcriterias.title as criteria
-                            from questionbank, subjects,evaluationcriterias
-                            where questionbank.subjectid=subjects.id and questionbank.evaluationcriteriaid=evaluationcriterias.id
-                            and subjects.id=1 and evaluationcriterias.id=1;
+from questionbank, subjects,evaluationcriterias
+where questionbank.subjectid=subjects.id and questionbank.evaluationcriteriaid=evaluationcriterias.id
+and subjects.id=1 and evaluationcriterias.id=1;
                             
 select employees.firstname,employees.lastname,interviews.candidateid from employees
 inner join interviews
@@ -74,47 +73,13 @@ on interviewcriterias.evaluationcriteriaid = evaluationcriterias.id
 where interviews.id=2;
             
 
-select candidateanswers.testquestionid,testquestions.testid,
-candidateanswers.answerkey as candidateanswer ,questionbank.answerkey
-from  candidateanswers
-INNER JOIN   testquestions  on testquestions.questionbankid=candidateanswers.testquestionid
-INNER JOIN   questionbank on questionbank.id=testquestions.questionbankid
-where candidateanswers.candidateid=4 and testquestions.testid=1;
-
-select candidatetestresults.testid,candidatetestresults.score,candidatetestresults.candidateid
-,employees.firstname,employees.lastname,subjects.title as subject
-from candidatetestresults
-inner join employees
-on employees.id=candidatetestresults.candidateid
-inner join tests
-on candidatetestresults.testid=tests.id
-inner join subjects
-on tests.subjectid=subjects.id
-where candidatetestresults.testid=1;
 
 
-select candidatetestresults.testid, candidatetestresults.candidateid, employees.firstname, employees.lastname
-from candidatetestresults 
-inner join employees 
-on employees.id= candidatetestresults.candidateid
-where candidatetestresults.testid=1;
 
 
-select tests.id,candidatetestresults.candidateid,candidatetestresults.score,tests.passinglevel,employees.firstname,employees.lastname
-from tests
-inner join candidatetestresults
-on tests.id=candidatetestresults.testid
-inner join employees
-on candidatetestresults.candidateid=employees.id
-where candidatetestresults.score >= tests.passinglevel AND tests.id=1;
 
-select tests.id,candidatetestresults.candidateid,candidatetestresults.score,tests.passinglevel,employees.firstname,employees.lastname
-from tests
-inner join candidatetestresults
-on tests.id=candidatetestresults.testid
-inner join employees
-on candidatetestresults.candidateid=employees.id
-where candidatetestresults.score <= tests.passinglevel AND tests.id=1;
+
+
 
 
 
@@ -208,6 +173,30 @@ inner join candidatetestresults on tests.id=candidatetestresults.testid
 inner join employees on candidatetestresults.candidateid=employees.id
 where candidatetestresults.score < tests.passinglevel AND tests.id=1;
 
+-- get the candidateanswers and correctanswers
+select candidateanswers.testquestionid,testquestions.testid,
+candidateanswers.answerkey as candidateanswer,questionbank.answerkey
+from  candidateanswers
+INNER JOIN   testquestions  on testquestions.questionbankid=candidateanswers.testquestionid
+INNER JOIN   questionbank on questionbank.id=testquestions.questionbankid
+where candidateanswers.candidateid=1 and testquestions.testid=1;
+
+-- get candidatestresultdetails
+select candidatetestresults.testid,candidatetestresults.score,candidatetestresults.candidateid
+,employees.firstname,employees.lastname,subjects.title as subject
+from candidatetestresults inner join employees on employees.id=candidatetestresults.candidateid
+inner join tests on candidatetestresults.testid=tests.id
+inner join subjects on tests.subjectid=subjects.id
+where candidatetestresults.testid=1;
+
+-- get appearedcandidates
+select candidatetestresults.testid, candidatetestresults.candidateid, 
+employees.firstname, employees.lastname
+from candidatetestresults 
+inner join employees 
+on employees.id= candidatetestresults.candidateid
+where candidatetestresults.testid=1;
+
 -- to calculate score of given candidateid and testid;
 call spcandidatetestresult(2,1,@pscore) ;
 select(@pscore);
@@ -218,6 +207,9 @@ select @pcorrectAnswers,@pincorrectAnswers,@pskippedQuestions;
 
 -- get interviewdetails where interviewid=2
 call spinterviewdetails(2);
+
+
+
 
 
 
