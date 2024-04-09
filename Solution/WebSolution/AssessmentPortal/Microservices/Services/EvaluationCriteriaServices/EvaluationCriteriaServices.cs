8e974b64bbc8f2712ec8ce87167261e4f6e142dc
Ajinkya <ajinkyatambade143@gmail.com>
2024-04-09 13:50:21 +0530

    method added

---
+++ Solution/WebSolution/AssessmentPortal/Microservices/Services/EvaluationCriteriaServices/EvaluationCriteriaServices.cs
@@ -0,0 +1,43 @@
+﻿ using MySql.Data.MySqlClient;
+using System.Data;
+using EvaluationCriteriaEntities;
+using EvaluationCriteriaInterfaces;
+namespace EvaluationCriteriaServices;
+
+ 
+ public class EvaluationCriteriaService:IEvaluationCriteriaService
+{
+    private string connectionString = "server=localhost;port=3306;user=root;password=password;database=assessmentdb";
+ 
+ public bool UpdateCriteria(int evaluationCriteriaId, int questionId)
+    {
+
+        bool status = false;
+        MySqlConnection connection = new MySqlConnection(connectionString);
+        string query = "update questionbank set evaluationcriteriaid=@evaluationCriteriaId where id=@questionId";
+
+        MySqlCommand command = new MySqlCommand(query, connection);
+
+        command.Parameters.AddWithValue("@evaluationCriteriaId", evaluationCriteriaId);
+        command.Parameters.AddWithValue("@questionId", questionId);
+
+        try
+        {
+            connection.Open();
+            int rowsAffected = command.ExecuteNonQuery();
+            if (rowsAffected > 0)
+            {
+                status = true;
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            connection.Close();
+        }
+        return status;
+    }
+}
