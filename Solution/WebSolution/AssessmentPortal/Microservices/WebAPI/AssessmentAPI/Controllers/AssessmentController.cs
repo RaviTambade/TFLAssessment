aea4f24e40a1a333461bd0ed40235dcc0aa6a48d
Ajinkya <ajinkyatambade143@gmail.com>
2024-05-03 17:53:11 +0530

    Changes

--- Solution/WebSolution/AssessmentPortal/Microservices/WebAPI/AssessmentAPI/Controllers/AssessmentController.cs
+++ Solution/WebSolution/AssessmentPortal/Microservices/WebAPI/AssessmentAPI/Controllers/AssessmentController.cs
@@ -18,7 +18,6 @@ public class AssessmentController : ControllerBase
 
     }
     
-
     // GET: api/assessment
     [HttpGet("creationdate/fromDate/{fromDate}/toDate/{toDate}")]
     public async Task<IActionResult> GetAll(DateTime fromDate, DateTime toDate)
