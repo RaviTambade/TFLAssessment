5d6e1fe1478130a5d482c7c41aa8778ea013bccd
Ajinkya <ajinkyatambade143@gmail.com>
2024-05-14 15:54:15 +0530

    Dapper in EvaluationCriteria Method Modified

--- Solution/WebSolution/AssessmentPortal/Microservices/EvaluationCriteriaAPI/Repositories/Dapper/EvaluationCriteriaDapperRepository.cs
+++ Solution/WebSolution/AssessmentPortal/Microservices/EvaluationCriteriaAPI/Repositories/Dapper/EvaluationCriteriaDapperRepository.cs
@@ -7,15 +7,15 @@ using System.Data.SqlClient;
 using Dapper;
 using Org.BouncyCastle.Asn1.X509;
 
-public class EvaluationCriteriaDapperRepository:IEvaluationCriteriaRepository
-{ 
+public class EvaluationCriteriaDapperRepository : IEvaluationCriteriaRepository
+{
     private readonly IConfiguration _configuration;
     private readonly string _connectionString;
 
     public EvaluationCriteriaDapperRepository(IConfiguration configuration)
     {
         _configuration = configuration;
-        _connectionString = _configuration.GetConnectionString("DefaultConnection")  ?? throw new ArgumentNullException("connectionString");
+        _connectionString = _configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("connectionString");
     }
     public async Task<bool> UpdateCriteria(int evaluationCriteriaId, int questionId)
     {
@@ -31,88 +31,34 @@ public class EvaluationCriteriaDapperRepository:IEvaluationCriteriaRepository
         return status;
     }
 
-    public async Task <bool> UpdateSubject(int id, int subjectId)
+    public async Task<bool> UpdateSubject(int id, int subjectId)
     {
 
-        // bool status = false;
-        // MySqlConnection connection = new MySqlConnection(_connectionString );
-        // string query = "update evaluationcriterias set subjectid= @subjectId where id= @id;";
-        // MySqlCommand command = new MySqlCommand(query, connection);
-        // command.Parameters.AddWithValue("@id", id);
-        // command.Parameters.AddWithValue("@subjectId", subjectId);
-        // try
-        // {
-        //     await connection.OpenAsync();
-        //     int rowsAffected = command.ExecuteNonQuery();
-        //     if (rowsAffected > 0)
-        //     {
-        //         status = true;
-        //     }
-        // }
-        // catch (Exception e)
-        // {
-        //     Console.WriteLine(e.Message);
-        // }
-        // finally
-        // {
-        //     await connection.CloseAsync();
-        // }
-         await Task.Delay(100);
+        await Task.Delay(100);
         bool status = false;
         using (IDbConnection con = new MySqlConnection(_connectionString))
         {
-            string query = "UPDATE subjects SET subjectid = @SubjectId WHERE id = @Id";
-            con.Execute(query, new { Id = id, SubjectId = subjectId });
+            string query = "UPDATE evaluationcriterias SET subjectId = @subjectId WHERE id = @Id";
+            con.Execute(query, new { Id = id, subjectId = subjectId });
             status = true;
 
         }
         return status;
-       
+
     }
 
 
-    public async Task <bool> InsertCriteria(EvaluationCriteria criteria)
+    public async Task<bool> InsertCriteria(EvaluationCriteria criteria)
     {
-        Console.WriteLine(criteria.SubjectId + " " + criteria.Title);
-        // bool status = false;
-        // MySqlConnection connection = new MySqlConnection(_connectionString );
-        // string query = "insert into evaluationcriterias(title,subjectid) values ( @title, @subjectId)";
-        // MySqlCommand command = new MySqlCommand(query, connection);
-        // command.Parameters.AddWithValue("@subjectId", criteria.SubjectId);
-        // command.Parameters.AddWithValue("@title", criteria.Title);
-
-        // try
-        // {
-        //     await connection.OpenAsync();
-        //     int rowsAffected = command.ExecuteNonQuery();
-        //     if (rowsAffected > 0)
-        //     {
-        //         status = true;
-        //     }
-        // }
-        // catch (Exception e)
-        // {
-        //     Console.WriteLine(e.Message);
-        // }
-        // finally
-        // {
-        //    await connection.CloseAsync();
-        // }
         await Task.Delay(100);
         bool status = false;
-        string query = "INSERT INTO evaluationcriterias(subjectid,title) VALUES (@subjectid,@title)";
-       
-        
-        
-
         using (IDbConnection con = new MySqlConnection(_connectionString))
         {
-            if(con.Execute(query,new {subjectid= criteria.SubjectId,title=criteria.Title})  > 0)
-            status=true;
+            string query = "INSERT INTO evaluationcriterias(subjectid,title) VALUES (@subjectid,@title)";
+            if (con.Execute(query, new { subjectid = criteria.SubjectId, title = criteria.Title }) > 0)
+                status = true;
         }
-
-
-         return status;
+        return status;
     }
 
 }
