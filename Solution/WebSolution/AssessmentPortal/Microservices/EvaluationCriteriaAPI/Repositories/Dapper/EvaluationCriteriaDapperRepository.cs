e5ed184e7491d380966e1c6d6acd0ead52811bed
Ajinkya <ajinkyatambade143@gmail.com>
2024-05-14 13:00:04 +0530

    Dapper in EvaluationCriteria Method

--- Solution/WebSolution/AssessmentPortal/Microservices/EvaluationCriteriaAPI/Repositories/Dapper/EvaluationCriteriaDapperRepository.cs
+++ Solution/WebSolution/AssessmentPortal/Microservices/EvaluationCriteriaAPI/Repositories/Dapper/EvaluationCriteriaDapperRepository.cs
@@ -3,50 +3,31 @@ using System.Data;
 using Transflower.TFLAssessment.Entities;
 using Transflower.TFLAssessment.Repositories.Interfaces;
 namespace Transflower.TFLAssessment.Repositories;
+using System.Data.SqlClient;
+using Dapper;
 
 
  
- public class EvaluationCriteriaRepository:IEvaluationCriteriaRepository
+ public class EvaluationCriteriaDapperRepository:IEvaluationCriteriaRepository
 { 
     private readonly IConfiguration _configuration;
     private readonly string _connectionString;
 
-    public EvaluationCriteriaRepository(IConfiguration configuration)
+    public EvaluationCriteriaDapperRepository(IConfiguration configuration)
     {
         _configuration = configuration;
         _connectionString = _configuration.GetConnectionString("DefaultConnection")  ?? throw new ArgumentNullException("connectionString");
     }
-
-
-    
-  
-    
- public async Task <bool> UpdateCriteria(int evaluationCriteriaId, int questionId)
+    public async Task<bool> UpdateCriteria(int evaluationCriteriaId, int questionId)
     {
-
+        await Task.Delay(100);
         bool status = false;
-        MySqlConnection connection = new MySqlConnection(_connectionString );
-        string query = "update questionbank set evaluationcriteriaid=@evaluationCriteriaId where id=@questionId";
-        MySqlCommand command = new MySqlCommand(query, connection);
-        command.Parameters.AddWithValue("@evaluationCriteriaId", evaluationCriteriaId);
-        command.Parameters.AddWithValue("@questionId", questionId);
+        using (IDbConnection con = new MySqlConnection(_connectionString))
+        {
+            string query = "UPDATE questionbank SET evaluationcriteriaid = @EvaluationCriteriaId WHERE id = @QuestionId";
+            con.Execute(query, new { EvaluationCriteriaId = evaluationCriteriaId, QuestionId = questionId });
+            status = true;
 
-        try
-        {
-            await connection.OpenAsync();
-            int rowsAffected = command.ExecuteNonQuery();
-            if (rowsAffected > 0)
-            {
-                status = true;
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        finally
-        {
-            await connection.CloseAsync();
         }
         return status;
     }
