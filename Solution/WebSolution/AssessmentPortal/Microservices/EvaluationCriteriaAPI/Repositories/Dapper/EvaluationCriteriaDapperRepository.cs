88a5978af6d17fba639d9f97bdf892dd9266e15a
vaibhav-lakudzode <vaibhavlakudzode26@gmail.com>
2024-05-14 14:37:30 +0530

    evaluationcriteriaapi dapper

--- Solution/WebSolution/AssessmentPortal/Microservices/EvaluationCriteriaAPI/Repositories/Dapper/EvaluationCriteriaDapperRepository.cs
+++ Solution/WebSolution/AssessmentPortal/Microservices/EvaluationCriteriaAPI/Repositories/Dapper/EvaluationCriteriaDapperRepository.cs
@@ -5,10 +5,9 @@ using Transflower.TFLAssessment.Repositories.Interfaces;
 namespace Transflower.TFLAssessment.Repositories;
 using System.Data.SqlClient;
 using Dapper;
+using Org.BouncyCastle.Asn1.X509;
 
-
- 
- public class EvaluationCriteriaDapperRepository:IEvaluationCriteriaRepository
+public class EvaluationCriteriaDapperRepository:IEvaluationCriteriaRepository
 { 
     private readonly IConfiguration _configuration;
     private readonly string _connectionString;
@@ -35,30 +34,40 @@ using Dapper;
     public async Task <bool> UpdateSubject(int id, int subjectId)
     {
 
+        // bool status = false;
+        // MySqlConnection connection = new MySqlConnection(_connectionString );
+        // string query = "update evaluationcriterias set subjectid= @subjectId where id= @id;";
+        // MySqlCommand command = new MySqlCommand(query, connection);
+        // command.Parameters.AddWithValue("@id", id);
+        // command.Parameters.AddWithValue("@subjectId", subjectId);
+        // try
+        // {
+        //     await connection.OpenAsync();
+        //     int rowsAffected = command.ExecuteNonQuery();
+        //     if (rowsAffected > 0)
+        //     {
+        //         status = true;
+        //     }
+        // }
+        // catch (Exception e)
+        // {
+        //     Console.WriteLine(e.Message);
+        // }
+        // finally
+        // {
+        //     await connection.CloseAsync();
+        // }
+         await Task.Delay(100);
         bool status = false;
-        MySqlConnection connection = new MySqlConnection(_connectionString );
-        string query = "update evaluationcriterias set subjectid= @subjectId where id= @id;";
-        MySqlCommand command = new MySqlCommand(query, connection);
-        command.Parameters.AddWithValue("@id", id);
-        command.Parameters.AddWithValue("@subjectId", subjectId);
-        try
+        using (IDbConnection con = new MySqlConnection(_connectionString))
         {
-            await connection.OpenAsync();
-            int rowsAffected = command.ExecuteNonQuery();
-            if (rowsAffected > 0)
-            {
-                status = true;
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        finally
-        {
-            await connection.CloseAsync();
+            string query = "UPDATE subjects SET subjectid = @SubjectId WHERE id = @Id";
+            con.Execute(query, new { Id = id, SubjectId = subjectId });
+            status = true;
+
         }
         return status;
+       
     }
 
 
