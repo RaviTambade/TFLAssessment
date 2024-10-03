a587f42a32f637a45f2575d2ae65e669bb4d36e7
shri <patilshridhar8056@gmail.com>
2024-10-03 16:17:05 +0530

    react + assessmentAPI

---
+++ Solution/UISolution/tflAssessmentReact/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import AssessmentList from './components/TflAssessment/GetAllAssessment'
+
+function App() {
+  
+
+  return (
+    <>
+      <AssessmentList/>
+    </>
+  )
+}
+
+export default App;
