938c158d3fe7e09b64b1c8e0256bcc96cc0694a0
shubhangtambade <shubhangi.tambade@gmail.com>
2024-10-04 17:44:42 +0530

    EvaluationCriteria

--- Solution/UISolution/tflAssessmentReact/src/EvaluationCriteria/Component/Crud/InsertCriteria.jsx
+++ Solution/UISolution/tflAssessmentReact/src/EvaluationCriteria/Component/Crud/InsertCriteria.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 //import { useNavigate } from "react-router-dom";
 
-//import EvaluationCriteriaService from "../../Services/EvaluationCriteriaServiceFetch"
+import EvaluationCriteriaService from "../../Services/EvaluationCriteriaServiceFetch"
 
 const InsertCriteria = () => {
 
