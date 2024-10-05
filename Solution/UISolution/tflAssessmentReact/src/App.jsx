017def18694ed3438ff903f81de65772a8b7518f
AjinkyaBTambade <ajinkyatambade143@gmail.com>
2024-10-05 16:01:18 +0530

    changes

--- Solution/UISolution/tflAssessmentReact/src/App.jsx
+++ Solution/UISolution/tflAssessmentReact/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      {/* <AssessmentList/> */}
+    {/* <AssessmentList/> */}
     {/* <InterviewList/> */}
     {/* <InterviewSubjects/> */}
     {/* <InterviewDeatils/> */}
