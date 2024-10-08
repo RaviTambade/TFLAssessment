49f71b938c4f87cdba734a9281c24397039594c8
AjinkyaBTambade <ajinkyatambade143@gmail.com>
2024-10-08 11:36:37 +0530

    Changes

--- Solution/UISolution/tflAssessmentReact/src/App.jsx
+++ Solution/UISolution/tflAssessmentReact/src/App.jsx
@@ -10,13 +10,12 @@ import './App.css';
 //import InterviewDeatils from './InterviewDetails/components/GetInterviewDetails';
 //import GetCandidateResults from './AssementIntellegence/Components/GetCandidateResult';
 //import InsertCriteria from './EvaluationCriteria/Component/Crud/InsertCriteria';
-import QuestionBankList from './testAppear_Shubhangi/Component/AppearTest';
+//import QuestionBankList from './testAppear_Shubhangi/Component/AppearTest';
 //import ChangeInterviewerComponent from './InterviewDetails/components/ChangeInterviewer';
 //import CancelInterviewComponent from './InterviewDetails/components/CancelInterview';
-
-import InsertCriteria from './EvaluationCriteria/Component/Crud/InsertCriteria';
-import SubjectMatterExpertDetails from './Assessment/Components/GetAllBySME';
-import CreateTestComponent from './Assessment/Components/CreateNewTest';
+// import InsertCriteria from './EvaluationCriteria/Component/Crud/InsertCriteria';
+// import SubjectMatterExpertDetails from './Assessment/Components/GetAllBySME';
+// import CreateTestComponent from './Assessment/Components/CreateNewTest';
 import TestAppear from './Assessment/Components/TestAppear';
 function App() {
   
@@ -27,12 +26,10 @@ function App() {
     {/* <InterviewSubjects/> */}
     {/* <InterviewDeatils/> */}
     {/*<GetCandidateResults/>*/}
-
-     {/* <InsertCriteria/> */} 
-
-     <QuestionBankList/>
+    {/* <InsertCriteria/> */} 
+    {/* <QuestionBankList/> */}
    
-<TestAppear/>
+     <TestAppear/>
     </>
   )
 }
