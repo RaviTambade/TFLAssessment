878235ad7fd89a2cd661fa28fec5a1f7f609858b
AjinkyaBTambade <ajinkyatambade143@gmail.com>
2024-11-10 20:40:24 +0530

    Implementing Project

--- Solution/WebSolution/AssessmentPortal/Microservices/EmployeeAPI/Repositories/IEmployeeRepository.cs
+++
@@ -1,7 +0,0 @@
-using  Transflower.TFLAssessment.Entities;
-namespace Transflower.TFLAssessment.Repositories.Interfaces;
-
-public interface IEmployeeRepository
-{
-  public Task <Employee> GetDetails(int UserId);
-}
