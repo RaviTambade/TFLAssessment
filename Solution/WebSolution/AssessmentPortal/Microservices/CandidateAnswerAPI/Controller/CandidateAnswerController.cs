5fc0c6cb52981a17eb5dc6e6946ac62d18e86db5
KajalGhule <kajalghule111@gmail.com>
2025-06-04 11:44:59 +0530

    test submitted api work

--- Solution/WebSolution/AssessmentPortal/Microservices/CandidateAnswerAPI/Controller/CandidateAnswerController.cs
+++ Solution/WebSolution/AssessmentPortal/Microservices/CandidateAnswerAPI/Controller/CandidateAnswerController.cs
@@ -9,25 +9,38 @@ using Transflower.TFLAssessment.Services.Interfaces;
 [ApiController]
 [Route("api/candidateanswer")]
 public class CandidateAnswerController : ControllerBase
-{ 
-    private readonly ILogger <CandidateAnswerController> _logger;
+{
+    private readonly ILogger<CandidateAnswerController> _logger;
     private readonly ICandidateAnswerService _service;
     public CandidateAnswerController(ICandidateAnswerService service, ILogger<CandidateAnswerController> logger)
     {
         _service = service;
-        _logger= logger;
+        _logger = logger;
     }
-        
+
     // Insert candidate answers of the test .
     [HttpPost("assessmentanswers/candidates/{candidateId}")]
     public async Task<IActionResult> InsertCandidateAnswers(int candidateId, [FromBody] List<CandidateAnswer> answers)
     {
-        bool status =await _service.InsertCandidateAnswers(candidateId,answers);
-        if(!status) {
+        bool status = await _service.InsertCandidateAnswers(candidateId, answers);
+        if (!status)
+        {
             _logger.LogError("Failed to insert candidate answers for candidate ID: {CandidateId}", candidateId);
             return BadRequest("Failed to insert candidate answers.");
         }
         _logger.LogInformation("Candidate answers inserted successfully for candidate ID: {CandidateId}", candidateId);
-        return Ok(new {message ="Candidate answers inserted successfully.",status = status});
+        return Ok(new { message = "Candidate answers inserted successfully.", status = status });
     }
+    
+    // [HttpPost("assessmentanswers/candidates/{candidateId}/testId/{testId}")]
+    // public async Task<IActionResult> InsertCandidateAnswers(int candidateId, [FromBody] List<CandidateAnswer> answers)
+    // {
+    //     bool status =await _service.InsertCandidateAnswers(candidateId,answers);
+    //     if(!status) {
+    //         _logger.LogError("Failed to insert candidate answers for candidate ID: {CandidateId}", candidateId);
+    //         return BadRequest("Failed to insert candidate answers.");
+    //     }
+    //     _logger.LogInformation("Candidate answers inserted successfully for candidate ID: {CandidateId}", candidateId);
+    //     return Ok(new {message ="Candidate answers inserted successfully.",status = status});
+    // }
 }
