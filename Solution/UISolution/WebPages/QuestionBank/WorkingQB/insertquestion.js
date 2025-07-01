0c57744c664c7f229e182fb7076cc781a62c6e00
KajalGhule <kajalghule111@gmail.com>
2025-07-01 10:44:14 +0530

    add new question UI

--- Solution/UISolution/WebPages/QuestionBank/WorkingQB/insertquestion.js
+++ Solution/UISolution/WebPages/QuestionBank/WorkingQB/insertquestion.js
@@ -1,124 +1,102 @@
-
 var subjectsApi = "http://localhost:5151/api/assessment/subjects";
 var criteriaApi = "http://localhost:5151/api/assessment/criterias";
 var subjects = [];
 var criterias = [];
-var question = [];
 var subjectId;
 var criteriaId;
-$(document).ready(function () {
 
+$(document).ready(function () {
   var insertquestionurl = "http://localhost:5172/api/questionbank/question";
 
+  // Load subjects
   $.ajax({
     url: subjectsApi,
     type: 'GET',
     contentType: 'application/json',
-
     success: function (response) {
       subjects = response;
-      console.log(subjects);
-      var lstSubject = $("#ddlSubjects")
-      for (var i = 0; i < subjects.length; i++) {
-        lstSubject.append($('<option></option>').val(subjects[i].id).html(subjects[i].title));
-        
-      }
-      var apiCriteriasBySubject = criteriaApi + "/subjects/" + subjects[0].id;
-      console.log(apiCriteriasBySubject);
-
-      $.ajax({
-        url: apiCriteriasBySubject,
-        type: 'GET',
-        contentType: 'application/json',
-        success: function (data) {
-          criterias = data;
-          console.log(criterias);
-
-          var lstCriteria = $("#ddlCriterias")
-          for (var i = 0; i < criterias.length; i++) {
-            lstCriteria.append($('<option></option>').val(criterias[i].id).html(criterias[i].title));
-          }
-        },
-        error: function (xhr, status, error) {
-          console.error(xhr.responseText);
-        }
+      var lstSubject = $("#ddlSubjects");
+      lstSubject.append(`<option disabled selected>Choose Subject</option>`);
+      subjects.forEach(subject => {
+        lstSubject.append($('<option></option>').val(subject.id).html(subject.title));
       });
 
+      // Load criteria for first subject
+      loadCriteria(subjects[0].id);
     },
-    error: function (xhr, status, error) {
+    error: function (xhr) {
       console.error(xhr.responseText);
     }
   });
 
+  // Subject change handler
   $("#ddlSubjects").change(function () {
-    $('#ddlCriterias').empty();
+    subjectId = $(this).val();
+    loadCriteria(subjectId);
+  });
 
-    subjectId = $("#ddlSubjects").val();
-    console.log(subjectId);
+  function loadCriteria(subjectId) {
+    $("#ddlCriterias").empty().append(`<option disabled selected>Choose Criteria</option>`);
 
-    var apiCriteriasBySubject = criteriaApi + "/subjects/" + subjectId;
+    var apiCriteriasBySubject = `${criteriaApi}/subjects/${subjectId}`;
     $.ajax({
       url: apiCriteriasBySubject,
       type: 'GET',
       contentType: 'application/json',
       success: function (data) {
         criterias = data;
-        var lstCriteria = $("#ddlCriterias")
-        for (var i = 0; i < criterias.length; i++) {
-          lstCriteria.append($('<option></option>').val(criterias[i].id).html(criterias[i].title));
-        }
-        criteriaId = $("#ddlCriterias").val();
-        console.log(criteriaId);
+        var lstCriteria = $("#ddlCriterias");
+        criterias.forEach(criteria => {
+          lstCriteria.append($('<option></option>').val(criteria.id).html(criteria.title));
+        });
       },
-      error: function (xhr, status, error) {
+      error: function (xhr) {
         console.error(xhr.responseText);
       }
     });
-  });
+  }
 
-
-
+  // Insert Question
   $("#insertButton").click(function () {
-    var skillId = subjectId;
-    var question = $("#question").val();
-    var optionA = $("#A").val();
-    var optionB = $("#B").val();
-    var optionC = $("#C").val();
-    var optionD = $("#D").val();
-    var correctOption = $("#correctOption").val();
-    var evaluationCriteriaId = criteriaId;
+    subjectId = $("#ddlSubjects").val();
+    criteriaId = $("#ddlCriterias").val();
 
     var newQuestion = {
-      "subjectId": skillId,
-      "title": question,
-      "a": optionA,
-      "b": optionB,
-      "c": optionC,
-      "d": optionD,
-      "answerKey": correctOption,
-      "evaluationCriteriaId": evaluationCriteriaId
+      subjectId: subjectId,
+      title: $("#question").val(),
+      a: $("#A").val(),
+      b: $("#B").val(),
+      c: $("#C").val(),
+      d: $("#D").val(),
+      answerKey: $("#correctOption").val(),
+      evaluationCriteriaId: criteriaId
     };
-
-    console.log("skillid: " + skillId);
-    console.log("Question: " + question);
-    console.log("Option A: " + optionA);
-    console.log("Option B: " + optionB);
-    console.log("Option C: " + optionC);
-    console.log("Option D: " + optionD);
-    console.log("Correct Option: " + correctOption);
-    console.log("evaluationCriteriaId: " + evaluationCriteriaId);
 
     $.ajax({
       url: insertquestionurl,
       type: 'POST',
       contentType: 'application/json',
       data: JSON.stringify(newQuestion),
-
       success: function (response) {
-        // Show result
-        console.log(response);
+        alert("Question inserted successfully!");
+
+        // Clear all fields
+        $("#ddlSubjects").prop('selectedIndex', 0);
+        $("#ddlCriterias").empty().append(`<option disabled selected>Choose Criteria</option>`);
+        $("#question").val('');
+        $("#A").val('');
+        $("#B").val('');
+        $("#C").val('');
+        $("#D").val('');
+        $("#correctOption").val('');
+
+        // Reload criteria for selected subject
+        var currentSubject = $("#ddlSubjects").val();
+        if (currentSubject) {
+          loadCriteria(currentSubject);
+        }
       },
-      error: function (xhr, status, error) {
+      error: function (xhr) {
         console.error(xhr.responseText);
       }
     });
