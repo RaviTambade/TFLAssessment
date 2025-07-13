b6a7799ba466d78d29fc6856304b066883a00550
Pankaj Bhor <pankajbhor373@gmail.com>
2025-07-13 10:41:56 +0530

    User input taken

--- Solution/Java/PankajB/DBOOPS/src/main/java/com/tap/pankaj/UI/UIManager.java
+++ Solution/Java/PankajB/DBOOPS/src/main/java/com/tap/pankaj/UI/UIManager.java
@@ -1,45 +1,84 @@
 package com.tap.pankaj.UI;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 import com.tap.pankaj.demo.App;
+import com.tap.pankaj.entity.data;
 public class UIManager {
     
     public Scanner scanner = new Scanner(System.in);
     public void displayMenu() {
-        System.out.println("****Menu for database connectivity operations****");
-            System.out.println("1. Create");
-            System.out.println("2. Insert");
-            System.out.println("3. Update");
-            System.out.println("4. Delete");
-            System.out.println("5. Select");
-            System.out.println("6. Truncate");
-            System.out.println("7. Alter");
-            System.out.println("8. Drop");
-            System.out.println("9. Exit");
+        System.out.println();
+        System.out.println("****Menu for Database Connectivity Operations****");
+        System.out.println();
+        System.out.println("1. Create");
+        System.out.println("2. Insert");
+        System.out.println("3. Update");
+        System.out.println("4. Delete");
+        System.out.println("5. Select");
+        System.out.println("6. Truncate");
+        System.out.println("7. Alter");
+        System.out.println("8. Drop");
+        System.out.println("9. Exit");
+    }
+    public data getData() {
+        data data1 = new data();
+        switch(App.choice) {
+
+            case 2: {
+                System.out.println( "Enter Name: ");
+                data1.setName(scanner.nextLine());
+                System.out.println();
+                break;
+            }
+
+            case 3: {
+                System.out.println( "Enter ID: ");
+                data1.setId(scanner.nextInt());
+                scanner.nextLine();
+                System.out.println( "Enter Name: ");
+                data1.setName(scanner.nextLine());
+                System.out.println();                
+                break;
+            }
+
+            case 4: {
+                System.out.println( "Enter ID: ");
+                data1.setId(scanner.nextInt());
+                scanner.nextLine();
+                break;
+            }
+        }
+        return data1;
     }
 
     public int getChoice() {
-            System.out.println("Enter your choice");
+            System.out.println();
+            System.out.print("Enter your choice: ");
             return scanner.nextInt();
     }
 
-    public static int displaySelectResult(ResultSet result) {
-        int count = 0;                                                                          
+    public static void displaySelectResult(ResultSetMetaData metadata, ResultSet result, int columnCount) {
         try {
+            System.out.println("");
+            System.out.println("----------------------------------------");
+            for (int i=1; i<=columnCount; i++) {
+                System.out.printf("%-20s", metadata.getColumnName(i));
+            }
+            System.out.println();
             while (result.next()) {
-                for (int i = 1; i <= 2; i++) {                                  
+                for (int i = 1; i <= columnCount; i++) {                                  
                     System.out.printf("%-20s", result.getString(i));
                 }
-                count++;
                 System.out.println();
             }
-            return count;
+            System.out.println("----------------------------------------");
+            System.out.println();
         }catch(SQLException e) {
             System.out.println(e);
-            return -1;
         }
     }
 
