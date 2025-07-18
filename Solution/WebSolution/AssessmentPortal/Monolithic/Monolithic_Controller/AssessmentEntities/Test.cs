2cf2aeabce4ab01bf39b1360e187c5a14cfa6520
KajalGhule <kajalghule111@gmail.com>
2025-07-18 12:56:56 +0530

    change test status

--- Solution/WebSolution/AssessmentPortal/Monolithic/Monolithic_Controller/AssessmentEntities/Test.cs
+++ Solution/WebSolution/AssessmentPortal/Monolithic/Monolithic_Controller/AssessmentEntities/Test.cs
@@ -3,6 +3,7 @@ namespace Transflower.TFLAssessment.Entities;
 
 public class Test
 { 
+    public int Id { get; set; }
     public string Name { get; set; }
     public DateTime ScheduledDate { get; set; }
     public string Status { get; set; }
