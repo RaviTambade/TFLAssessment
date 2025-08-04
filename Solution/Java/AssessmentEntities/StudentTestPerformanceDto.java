e9b9705848fbbc43a111d21e95e7d1c174cf923e
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-08-04 17:54:57 +0530

    java entities

---
+++ Solution/Java/AssessmentEntities/StudentTestPerformanceDto.java
@@ -0,0 +1,18 @@
+package com.transflower.tflassessment.Entities;
+import java.util.*;
+public class StudentTestPerformanceDto
+{
+    private List<TestScoreDto> testScores;
+
+
+    public List<TestScoreDto> getTestScores()
+    {
+        return testScores;
+    }
+
+    public void setTestScores(List<TestScoreDto> testScores)
+    {
+        this.testScores = testScores;
+    }
+}
+
---
+++ Solution/Java/AssessmentEntities/Subject.java
@@ -0,0 +1,25 @@
+package com.transflower.tflassessment.Entities;
+
+public class Subject {
+    private int Id;
+    private String Title;
+
+
+public int getId()
+{
+    return Id;
+}
+public void setId(int Id)
+{
+    this.Id=Id;
+}
+public String getTitle()
+{
+    return Title;
+}
+
+public void setTitle(String Title)
+{
+    this.Title=Title;
+}
+}
