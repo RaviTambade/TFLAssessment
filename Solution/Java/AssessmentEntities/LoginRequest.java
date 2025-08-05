c310a9c8a717ea58629e3b98063eb50dbda720ed
Yash Gawade <yashgavde9@gmail.com>
2025-08-05 10:15:01 +0530

    Update LoginRequest.java

--- Solution/Java/AssessmentEntities/LoginRequest.java
+++ Solution/Java/AssessmentEntities/LoginRequest.java
@@ -1,9 +1,11 @@
 package tap.transflower.tflAssessment.entities;
 
+import java.util.Objects;
+
 public class LoginRequest {
     
-    private String email;
-    private String password ;
+    private string email;
+    private string password ;
 
 
     public String getemail(){
@@ -21,6 +23,47 @@ public class LoginRequest {
     public void setpassword(String password){
         this. password= password;
     }
+
+
+
+     @Override
+     public String toString() {
+         return "LoginRequest{email="+ email+ ", password = " + password +"}";
+     }
+
+
+
+     @Override
+    public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+     LoginRequest other = ( LoginRequest) obj;
+        return Objects.equals(email, other.email) && Objects.equals(password, other.password);
+      }
+
+@Override
+public int hashCode() {
+    return Objects.hash(email, password);
+}
+
+@Override
+protected void finalize() throws Throwable {
+try {
+ } finally {
+ super.finalize();
+}
+}
+
+
+@Override
+protected Object clone() throws CloneNotSupportedException {
+    return super.clone();
+}
+
+
+
+}
+
 
 
 
