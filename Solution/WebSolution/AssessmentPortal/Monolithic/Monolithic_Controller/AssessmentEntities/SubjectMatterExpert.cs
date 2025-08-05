c714c4623fca3a1a10c64b179ed52bfb9dc4d59d
Sahil161967 <sahilbajkamble@gmail.com>
2025-08-05 18:39:38 +0530

    Updated entities

--- Solution/WebSolution/AssessmentPortal/Monolithic/Monolithic_Controller/AssessmentEntities/SubjectMatterExpert.cs
+++ Solution/WebSolution/AssessmentPortal/Monolithic/Monolithic_Controller/AssessmentEntities/SubjectMatterExpert.cs
@@ -13,3 +13,59 @@ namespace Transflower.TFLAssessment.Entities
         public Employee Employee { get; set; }
     }
 }
+
+
+package com.transflower.tflAssesment.entities;
+
+public class SubjectMatterExpert {
+
+    private int Id;
+    private int SubjectId;
+    private int EmployeeId;
+    private Subject Subject;
+    private Employee Employee;
+
+    public int getId(int Id)
+    {
+        return Id;
+    }
+
+    public void setId()
+    {
+        this.Id = Id;
+    }
+
+    public int getSubjectId(int SubjectId)
+    {
+        return SubjectId;
+    }
+    public void setSubjectId()
+    {
+        this.SubjectId = SubjectId;
+    }
+    public int getEmployeeId(int EmployeeId)
+    {
+        return EmployeeId;
+    }
+    public int setEmployeeId()
+    {
+        this.EmployeeId = EmployeeId;
+    }
+    public Subject getSubject(String Subject)
+    {
+        return Subject;
+    }
+    public Subject setSubject()
+    {
+        this.Subject = Subject;
+    }
+    public Employee getEmployee(Employee Employee)
+    {
+        return Employee;
+    }
+    public Employee setEmployee()
+    {
+        this.Employee = Employee;
+    }
+}
+
