79568f9ccb6a1d749336dc3457f786044c728e5e
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-08-06 09:29:10 +0530

    Resporistory Class

---
+++ Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/repositories/AssessmentIntelligenceRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.transflower.tflassessment.demo.repositories;
+
+import java.util.Properties;
+
+public class AssessmentIntelligenceRepositoryImpl implements AssessmentIntelligenceRepository {
+
+    
+    public AssessmentIntelligenceRepositoryImpl()
+    {
+        
+    }
+
+}
