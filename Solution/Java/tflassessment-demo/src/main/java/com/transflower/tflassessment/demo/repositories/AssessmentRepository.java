1d80d80451820da6bfcaf5a54ad2694c89afd5fe
YashGawade <yashgavde9@gmail.com>
2025-08-20 11:04:05 +0530

    update

--- Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/repositories/AssessmentRepository.java
+++ Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/repositories/AssessmentRepository.java
@@ -1,13 +1,11 @@
 package com.transflower.tflassessment.demo.repositories;
+
 import java.util.*;
 import com.transflower.tflassessment.demo.entities.*;
 
-
 public interface AssessmentRepository {
-    
 
     public Assessment getDetails(int assessmentId);
-
 
     public List<Assessment> getAll(Date fromDate, Date toDate);
 
@@ -17,17 +15,19 @@ public interface AssessmentRepository {
 
     public List<Employee> getAllEmployees();
 
+    public Employee getEmployeeById(int userId);
+
     public List<Subject> getAllSubjects();
 
     public List<EvaluationCriteria> getEvaluationCriterias();
 
     public List<EvaluationCriteria> getEvaluationCriteriasBySubject(int subjectId);
 
-    public boolean createTest(Assessment newTest);
+    public boolean createTest(CreateTestRequest request);
 
     public boolean addQuestion(int assessmentId, int questionId);
 
-    public boolean addQuestions(int assessmentId, List<TestQuestion> questions);
+    public boolean addQuestions(int assessmentId, List<QuestionBank> questions);
 
     public boolean changeDuration(int assessmentId, String duration);
 
@@ -36,4 +36,24 @@ public interface AssessmentRepository {
     public boolean removeQuestion(int assessmentId, int questionId);
 
     public boolean removeQuestions(int[] testQuestions);
+
+    public int createTestWithQuestions(CreateTestWithQuestions createTestWithQuestions);
+
+    public List<SubjectQuestions> getAllQuestionsBySubject(int subjectId);
+
+    public List<Employee> getSmeBySubject(int subjectId);
+
+    public List<Test> getAllTests(Date fromDate, Date toDate);
+
+    public TestWithQuestions getTestDetails(int testId);
+
+    public List<Question> getQuestionsByEvaluationCriteriaId(int evaluationCriteriaId);
+
+    public boolean updateQuestion(Question question);
+
+    public boolean updateTestStatus(int testId, TestStatusUpdate status);
+
+    public boolean addEmployeesToTest(TestAssignmentRequest request);
+
+    public List<TestEmployeeDetails> getAllTestByEmpId(int empId);
 }
