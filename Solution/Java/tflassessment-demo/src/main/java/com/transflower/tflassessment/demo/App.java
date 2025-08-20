ed02a30f81dde487980af7f8d23031aa826484c5
= <tejalc8322@gmail.com>
2025-08-20 10:33:09 +0530

    changes

--- Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/App.java
+++ Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/App.java
@@ -7,30 +7,39 @@ import java.util.List;
 import com.transflower.tflassessment.demo.entities.InterviewCandidateDetails;
 import com.transflower.tflassessment.demo.entities.InterviewDetails;
 import com.transflower.tflassessment.demo.repositories.InterviewRepositoryImpl;
+import com.transflower.tflassessment.demo.repositories.GetAllSubjectsImpl;
+import com.transflower.tflassessment.demo.repositories.ResultRepositoryImpl;
+
+import static com.transflower.tflassessment.demo.repositories.ResultRepositoryImpl.status1;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Welcome to transflower");
-        InterviewRepositoryImpl obj1 = new InterviewRepositoryImpl();
-        List<InterviewCandidateDetails> icd1 = obj1.getAllInterviewCandidates();
-        List<InterviewCandidateDetails> icd2 = obj1.getInterviewedCandidatesSubjects(6);
-        InterviewDetails id = obj1.getInterviewDetails(4);
-        boolean status1 = obj1.rescheduleInterview(4, LocalDate.of(2022, 12, 23));
-        boolean status2 = obj1.rescheduleInterview(3, LocalTime.of(12, 00), LocalDate.of(2032, 07, 23));
-        boolean status3 = obj1.rescheduleInterview(2, LocalDate.of(2022, 07, 23));
-        boolean status4 = obj1.cancelInterview(2);
-        boolean status5 = obj1.changeInterviewer(4, 1);
-
-        System.out.println("*");
-        System.out.println(id.toString());
-        System.out.println("*");
-        for (InterviewCandidateDetails icd : icd1) {
-            System.out.println(icd);
-        }
-        for (InterviewCandidateDetails icd : icd2) {
-            System.out.println(icd);
-        }
+         System.out.println("Welcome to transflower");
+         InterviewRepositoryImpl obj1 = new InterviewRepositoryImpl();
+         List<InterviewCandidateDetails> icd1 = obj1.getAllInterviewCandidates();
+         List<InterviewCandidateDetails> icd2 = obj1.getInterviewedCandidatesSubjects(6);
+         InterviewDetails id = obj1.getInterviewDetails(4);
+         boolean status1 = obj1.rescheduleInterview(4, LocalDate.of(2022, 12, 23));
+         boolean status2 = obj1.rescheduleInterview(3, LocalTime.of(12, 00), LocalDate.of(2032, 07, 23));
+         boolean status3 = obj1.rescheduleInterview(2, LocalDate.of(2022, 07, 23));
+         boolean status4 = obj1.cancelInterview(2);
+         boolean status5 = obj1.changeInterviewer(4, 1);
+System.out.println("Subjects from InterviewRepositoryImpl: " Subject2);
+        
+    
+        
+        
+        
+    //     System.out.println("*");
+    //     System.out.println(id.toString());
+    //     System.out.println("*");
+    //     for (InterviewCandidateDetails icd : icd1) {
+    //         System.out.println(icd);
+    //     }
+    //     for (InterviewCandidateDetails icd : icd2) {
+    //         System.out.println(icd);
+    //     }
     }
 }
