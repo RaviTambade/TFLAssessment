db47f8c6f99ca0ee88a6b42b1728d1aa91aaae5f
Shrutik Daundkar <shrutikdaundkar6225@gmail.com>
2025-08-20 10:38:05 +0530

    Update AssessmentRepository.java

--- Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/repositories/AssessmentRepository.java
+++ Solution/Java/tflassessment-demo/src/main/java/com/transflower/tflassessment/demo/repositories/AssessmentRepository.java
@@ -1,41 +1,39 @@
-// package com.transflower.tflassessment.demo.repositories;
-// import java.util.*;
-// import com.transflower.tflassessment.demo.entities.*;
+package com.transflower.tflassessment.demo.repositories;
+import java.util.*;
+import com.transflower.tflassessment.demo.entities.*;
 
 
-// public interface AssessmentRepository {
+public interface AssessmentRepository {
     
-<<<<<<< HEAD
-    public List<Assessment> getDetails(int assessmentId);
-=======
-//     public Assessment getDetails(int assessmentId);
->>>>>>> 41afbd6b65e21b4b9f89c2b8c8b8a9f4f431476d
 
-//     public List<Assessment> getAll(Date fromDate, Date toDate);
+    public Assessment getDetails(int assessmentId);
 
-//     public List<Assessment> getAllTests();
 
-//     public List<Assessment> getAllBySubjectMatterExpert(int smeId);
+    public List<Assessment> getAll(Date fromDate, Date toDate);
 
-//     public List<Employee> getAllEmployees();
+    public List<Assessment> getAllTests();
 
-//     public List<Subject> getAllSubjects();
+    public List<Assessment> getAllBySubjectMatterExpert(int smeId);
 
-//     public List<EvaluationCriteria> getEvaluationCriterias();
+    public List<Employee> getAllEmployees();
 
-//     public List<EvaluationCriteria> getEvaluationCriteriasBySubject(int subjectId);
+    public List<Subject> getAllSubjects();
 
-//     public boolean createTest(Assessment newTest);
+    public List<EvaluationCriteria> getEvaluationCriterias();
 
-//     public boolean addQuestion(int assessmentId, int questionId);
+    public List<EvaluationCriteria> getEvaluationCriteriasBySubject(int subjectId);
 
-//     public boolean addQuestions(int assessmentId, List<TestQuestion> questions);
+    public boolean createTest(Assessment newTest);
 
-//     public boolean changeDuration(int assessmentId, String duration);
+    public boolean addQuestion(int assessmentId, int questionId);
 
-//     public boolean reschedule(int assessmentId, Date date);
+    public boolean addQuestions(int assessmentId, List<TestQuestion> questions);
 
-//     public boolean removeQuestion(int assessmentId, int questionId);
+    public boolean changeDuration(int assessmentId, String duration);
 
-//     public boolean removeQuestions(int[] testQuestions);
-// }
+    public boolean reschedule(int assessmentId, Date date);
+
+    public boolean removeQuestion(int assessmentId, int questionId);
+
+    public boolean removeQuestions(int[] testQuestions);
+}
