bc066ea0dd03a41ee9a652b3be07d2a493fd525f
sarthakkadam28 <sarthakkadam7777@gmail.com>
2025-08-28 22:11:59 +0530

    disconnectd fetch

---
+++ Solution/CSharp/Sarthak/MyConsoleApp/disconnectdfetch/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
+// Replace with your connection string
+string connectionString = "your_connection_string_here";
+string query = "SELECT * FROM YourTable";
+
+DataSet ds = new DataSet();
+
+using (SqlConnection conn = new SqlConnection(connectionString))
+{
+    // Create DataAdapter and command
+    SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+
+    // Fetch data into DataSet (disconnected)
+    adapter.Fill(ds);
+
+    // Now you can use ds.Tables with the fetched data; connection is closed!
+    foreach (DataRow row in ds.Tables.Rows)
+    {
+        Console.WriteLine(row["YourColumnName"]);
+    }
+}
+
