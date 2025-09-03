0bc9f5bde8c4688781666c3c16f7890e1db520cd
sanika-bhor <bhorsanika0239@gmail.com>
2025-09-03 15:23:17 +0530

    TFLAssessment project java version

---
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/demo/AssessmentApiApplication.java
@@ -0,0 +1,13 @@
+package com.transflower.tflassessment.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentApiApplication.class, args);
+	}
+
+}
---
+++ Solution/WebSolution/Java/AssessmentAPI/src/test/java/com/transflower/tflassessment/demo/AssessmentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transflower.tflassessment.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
