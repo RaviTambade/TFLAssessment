91b2a6d6495a94669b70cafcdf345c67b01374b4
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-09-09 17:15:41 +0530

    New Changes

---
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/InterviewController.java
@@ -0,0 +1,64 @@
+package com.transflower.tflassessment.controllers;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.transflower.tflassessment.entities.InterviewCandidateDetails;
+import com.transflower.tflassessment.entities.InterviewDetails;
+import com.transflower.tflassessment.services.InterviewService;
+@RestController
+public class InterviewController {
+ 
+    @Autowired
+    private  InterviewService svc;
+
+
+    @GetMapping("details")
+    public List<InterviewCandidateDetails> getAllInterviewCandidates() {
+        return svc.getAllInterviewCandidates();
+    }
+
+    @GetMapping("candidate/{candidateId}")
+    public List<InterviewCandidateDetails> getInterviewedCandidatesSubjects(@PathVariable("candidateId")int candidateId) {
+       return svc.getInterviewedCandidatesSubjects(candidateId);
+    }
+
+    @GetMapping("{interviewId}")
+    public InterviewDetails getInterviewDetails(@PathVariable("interviewId")int interviewId) {
+        return svc.getInterviewDetails(interviewId);
+    }
+
+    @GetMapping("{interviewId}/date/{date}")
+    public boolean rescheduleInterview(@PathVariable("interviewId")int interviewId, @RequestBody LocalDate date) {
+       return svc.rescheduleInterview(interviewId, date);
+    }
+
+    @GetMapping("{interviewId}/time/{time}")
+    public boolean rescheduleInterview(@PathVariable("interviewId")int interviewId, @RequestBody LocalTime time) {
+       return svc.rescheduleInterview(interviewId, time);
+    }
+
+    @GetMapping(("{interviewId}/time/{time}/date/{date}"))
+    public boolean rescheduleInterview(@PathVariable("interviewId")int interviewId, @RequestBody LocalTime time,@RequestBody LocalDate date) {
+       return svc.rescheduleInterview(interviewId, date);
+    }
+
+    @GetMapping("(\"{interviewId}/subjectexperts/{smeId}\")")
+    public boolean changeInterviewer(@PathVariable("interviewId")int interviewId,@PathVariable("smeId") int smeId) {
+       return svc.changeInterviewer(interviewId, smeId);
+    }
+
+    @GetMapping("{interviewId}")
+    public boolean cancelInterview(@PathVariable("interviewId")int interviewId) {
+        return svc.cancelInterview(interviewId);
+    }
+    
+    
+}
