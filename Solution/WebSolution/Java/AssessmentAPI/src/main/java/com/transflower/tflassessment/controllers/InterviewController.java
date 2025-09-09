389a408764630a3b93d7582f8e303ef865cde700
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-09-09 17:33:50 +0530

    Executable code

--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/InterviewController.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/InterviewController.java
@@ -5,60 +5,58 @@ import java.time.LocalTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.transflower.tflassessment.entities.InterviewCandidateDetails;
 import com.transflower.tflassessment.entities.InterviewDetails;
 import com.transflower.tflassessment.services.InterviewService;
+
 @RestController
+@RequestMapping("/api/interviews")
 public class InterviewController {
- 
+
     @Autowired
-    private  InterviewService svc;
+    private InterviewService svc;
 
-
-    @GetMapping("details")
+    @GetMapping("/details")
     public List<InterviewCandidateDetails> getAllInterviewCandidates() {
         return svc.getAllInterviewCandidates();
     }
 
-    @GetMapping("candidate/{candidateId}")
-    public List<InterviewCandidateDetails> getInterviewedCandidatesSubjects(@PathVariable("candidateId")int candidateId) {
-       return svc.getInterviewedCandidatesSubjects(candidateId);
+    @GetMapping("/candidate/{candidateId}")
+    public List<InterviewCandidateDetails> getInterviewedCandidatesSubjects(@PathVariable int candidateId) {
+        return svc.getInterviewedCandidatesSubjects(candidateId);
     }
 
-    @GetMapping("{interviewId}")
-    public InterviewDetails getInterviewDetails(@PathVariable("interviewId")int interviewId) {
+    @GetMapping("/{interviewId}")
+    public InterviewDetails getInterviewDetails(@PathVariable int interviewId) {
         return svc.getInterviewDetails(interviewId);
     }
 
-    @GetMapping("{interviewId}/date/{date}")
-    public boolean rescheduleInterview(@PathVariable("interviewId")int interviewId, @RequestBody LocalDate date) {
-       return svc.rescheduleInterview(interviewId, date);
+    @PutMapping("/{interviewId}/date")
+    public boolean rescheduleInterviewDate(@PathVariable int interviewId, @RequestBody LocalDate date) {
+        return svc.rescheduleInterview(interviewId, date);
     }
 
-    @GetMapping("{interviewId}/time/{time}")
-    public boolean rescheduleInterview(@PathVariable("interviewId")int interviewId, @RequestBody LocalTime time) {
-       return svc.rescheduleInterview(interviewId, time);
+    @PutMapping("/{interviewId}/time")
+    public boolean rescheduleInterviewTime(@PathVariable int interviewId, @RequestBody LocalTime time) {
+        return svc.rescheduleInterview(interviewId, time);
     }
 
-    @GetMapping(("{interviewId}/time/{time}/date/{date}"))
-    public boolean rescheduleInterview(@PathVariable("interviewId")int interviewId, @RequestBody LocalTime time,@RequestBody LocalDate date) {
-       return svc.rescheduleInterview(interviewId, date);
+    @PutMapping("/{interviewId}/datetime")
+    public boolean rescheduleInterviewDateTime(@PathVariable int interviewId,
+                                               @RequestBody LocalDate date,
+                                               @RequestBody LocalTime time) {
+        return svc.rescheduleInterview(interviewId, date); // adjust logic in service if needed
     }
 
-    @GetMapping("(\"{interviewId}/subjectexperts/{smeId}\")")
-    public boolean changeInterviewer(@PathVariable("interviewId")int interviewId,@PathVariable("smeId") int smeId) {
-       return svc.changeInterviewer(interviewId, smeId);
+    @PutMapping("/{interviewId}/subjectexperts/{smeId}")
+    public boolean changeInterviewer(@PathVariable int interviewId, @PathVariable int smeId) {
+        return svc.changeInterviewer(interviewId, smeId);
     }
 
-    @GetMapping("{interviewId}")
-    public boolean cancelInterview(@PathVariable("interviewId")int interviewId) {
+    @DeleteMapping("/{interviewId}")
+    public boolean cancelInterview(@PathVariable int interviewId) {
         return svc.cancelInterview(interviewId);
     }
-    
-    
 }
--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/QuestionBankController.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/QuestionBankController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.transflower.tflassessment.entities.NewQuestion;
@@ -16,6 +17,7 @@ import com.transflower.tflassessment.entities.SubjectQuestion;
 import com.transflower.tflassessment.services.QuestionBankService;
 
 @RestController
+@RequestMapping("api/questionbank")
 public class QuestionBankController {
     
     @Autowired
