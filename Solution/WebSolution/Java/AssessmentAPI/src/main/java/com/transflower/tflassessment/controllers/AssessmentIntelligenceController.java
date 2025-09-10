b7980adf74bcfd184120f552dd218d42ba27e307
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-09-10 16:47:45 +0530

    executable controller

--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/AssessmentIntelligenceController.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/AssessmentIntelligenceController.java
@@ -5,20 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.transflower.tflassessment.entities.AnnualCandidateResult;
 import com.transflower.tflassessment.services.AssessmentIntelligenceService;
 
 @RestController
+@RequestMapping("/CandidateResults")  
 public class AssessmentIntelligenceController {
     
     @Autowired
     private AssessmentIntelligenceService svc;
 
-    @GetMapping("CandidateResults/candidateanswer/{candidateid}/testtime/{year}")
-        List <AnnualCandidateResult> getCandidateResults(@PathVariable("candidateid") int candidateId , @PathVariable("year") int year)
-        {
-            return svc.getCandidateResults(candidateId, year);
-        }
+    @GetMapping("/candidateanswer/{candidateid}/testtime/{year}")
+    public List<AnnualCandidateResult> getCandidateResults(@PathVariable("candidateid") int candidateId, @PathVariable("year") int year) {
+        
+        return svc.getCandidateResults(candidateId, year);
+    }
 }
