6203eb57f55b51b1bf0cb738a3c6de477ff8e67b
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2025-09-10 16:32:51 +0530

    executable controller

--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/EvaluationCriteriaController.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/EvaluationCriteriaController.java
@@ -1,16 +1,15 @@
 package com.transflower.tflassessment.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.transflower.tflassessment.entities.EvaluationCriteria;
 import com.transflower.tflassessment.services.EvaluationCriteriaService;
-
 
 @RestController
 @RequestMapping("api/criteria")
@@ -19,18 +18,18 @@ public class EvaluationCriteriaController {
     @Autowired
     private EvaluationCriteriaService svc;
 
-    @PostMapping("/eva/criteria")
-    public boolean postinsertCriteria(@RequestBody EvaluationCriteria criteria) {
+    @PostMapping
+    public boolean insertCriteria(@RequestBody EvaluationCriteria criteria) {
         return svc.insertCriteria(criteria);
     }
 
     @PutMapping("{id}/subjects/{subjectId}")
-    public boolean putupdateSubject(@RequestParam int id, @RequestParam int subjectId) {
+    public boolean updateSubject(@PathVariable("id") int id, @PathVariable("subjectId") int subjectId) {
         return svc.updateSubject(id, subjectId);
     }
 
     @PutMapping("{evaluationCriteriaId}/questions/{questionId}")
-    public boolean putupdateCriteria(@RequestParam int EvaluationCriteriaId, @RequestParam int questionId) {
-        return svc.updateCriteria(questionId, EvaluationCriteriaId);
+    public boolean updateCriteria(@PathVariable("evaluationCriteriaId") int evaluationCriteriaId,@PathVariable("questionId") int questionId) {
+        return svc.updateCriteria(evaluationCriteriaId, questionId);
     }
 }
--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/services/CandidateAnswerService.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/services/CandidateAnswerService.java
@@ -7,9 +7,8 @@ import com.transflower.tflassessment.entities.CandidateTestDetails;
 
 public interface CandidateAnswerService {
     
-    boolean insertCandidateAnswer(int candidateId, List <CandidateAnswer>answer);
-    List <CandidateAnswer>getCandidateAnswer(int CandidateId,int TestId);
-    List<CandidateAnswer>getCandidateAnswerResult(int CandidateId,int TestId);
-    CandidateTestDetails CandidateTestDetails (int CandidateId,int TestId);
-    
+    boolean insertCandidateAnswer(int candidateId, List<CandidateAnswer> answer);
+    List<CandidateAnswer> getCandidateAnswer(int CandidateId, int TestId);
+    List<CandidateAnswer> getCandidateAnswerResult(int CandidateId, int TestId);
+    CandidateTestDetails getCandidateTestDetails(int CandidateId, int TestId); 
 }
