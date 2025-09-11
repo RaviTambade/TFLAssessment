5abe850c323087af46491085e85b318c51159334
Sarthak Walake <sarthakwalake7424@gmail.com>
2025-09-11 22:16:53 +0530

    API

--- Solution/WebSolution/Java/AssessmentAPI/src/test/java/com/transflower/tflassessment/demo/AssessmentApiApplicationTests.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/test/java/com/transflower/tflassessment/demo/AssessmentApiApplicationTests.java
@@ -1,23 +1,13 @@
 // package com.transflower.tflassessment.demo;
 
-<<<<<<< HEAD
-import org.springframework.boot.test.context.SpringBootTest;
-=======
 // import org.junit.jupiter.api.Test;
 // import org.springframework.boot.test.context.SpringBootTest;
->>>>>>> 389a408764630a3b93d7582f8e303ef865cde700
 
 // @SpringBootTest
 // class AssessmentApiApplicationTests {
 
-<<<<<<< HEAD
-	// @Test
-	// void contextLoads() {
-	// }
-=======
 // 	@Test
 // 	void contextLoads() {
 // 	}
->>>>>>> 389a408764630a3b93d7582f8e303ef865cde700
 
 // }
