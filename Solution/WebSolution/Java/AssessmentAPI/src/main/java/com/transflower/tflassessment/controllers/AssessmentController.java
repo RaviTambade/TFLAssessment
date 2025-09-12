b0e1c4b86a38ffdad8e2762b4db8eacd75201db7
Sahil161967 <sahilbajkamble@gmail.com>
2025-09-12 16:08:00 +0530

    Async Assessment

--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/AssessmentController.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/controllers/AssessmentController.java
@@ -7,9 +7,28 @@ import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.transflower.tflassessment.entities.*;
+import com.transflower.tflassessment.entities.Assessment;
+import com.transflower.tflassessment.entities.CreateTestRequest;
+import com.transflower.tflassessment.entities.CreateTestWithQuestions;
+import com.transflower.tflassessment.entities.Employee;
+import com.transflower.tflassessment.entities.EvaluationCriteria;
+import com.transflower.tflassessment.entities.Question;
+import com.transflower.tflassessment.entities.QuestionBank;
+import com.transflower.tflassessment.entities.Subject;
+import com.transflower.tflassessment.entities.SubjectQuestion;
+import com.transflower.tflassessment.entities.Test;
+import com.transflower.tflassessment.entities.TestEmployeeDetails;
+import com.transflower.tflassessment.entities.TestEmployeeRequest;
+import com.transflower.tflassessment.entities.TestStatusUpdate;
+import com.transflower.tflassessment.entities.TestWithQuestions;
 import com.transflower.tflassessment.services.AssessmentService;
 
 @RestController
@@ -24,8 +43,7 @@ public class AssessmentController {
     }
 
     @GetMapping("/api/assessment/creationdate/fromDate/{fromDate}/toDate/{toDate}")
-    public CompletableFuture<List<Assessment>> getAll(
-            @PathVariable("fromDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
+    public CompletableFuture<List<Assessment>> getAll( @PathVariable("fromDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime fromDate,
             @PathVariable("toDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime toDate) {
         return svc.getAll(fromDate, toDate);
     }
@@ -56,8 +74,7 @@ public class AssessmentController {
     }
 
     @GetMapping("/api/assessment/criterias/subjects/{subjectId}")
-    public CompletableFuture<List<EvaluationCriteria>> getEvaluationCriteriasBySubject(
-            @PathVariable("subjectId") int subjectId) {
+    public CompletableFuture<List<EvaluationCriteria>> getEvaluationCriteriasBySubject(@PathVariable("subjectId") int subjectId) {
         return svc.getEvaluationCriteriasBySubject(subjectId);
     }
 
@@ -72,32 +89,27 @@ public class AssessmentController {
     }
 
     @PostMapping("/api/assessment/addquestion/assessments/{assessmentId}/questions/{questionId}")
-    public CompletableFuture<Boolean> addQuestion(@PathVariable("assessmentId") int assessmentId,
-                                                  @PathVariable("questionId") int questionId) {
+    public CompletableFuture<Boolean> addQuestion(@PathVariable("assessmentId") int assessmentId, @PathVariable("questionId") int questionId) {
         return svc.addQuestion(assessmentId, questionId);
     }
 
     @PostMapping("/api/assessment/addmultiplequestions/assessments/{assessmentId}")
-    public CompletableFuture<Boolean> addQuestions(@PathVariable("assessmentId") int assessmentId,
-                                                   @RequestBody List<QuestionBank> questions) {
+    public CompletableFuture<Boolean> addQuestions(@PathVariable("assessmentId") int assessmentId, @RequestBody List<QuestionBank> questions) {
         return svc.addQuestions(assessmentId, questions);
     }
 
     @DeleteMapping("/api/assessment/{assessmentId}/questions/{questionId}")
-    public CompletableFuture<Boolean> removeQuestion(@PathVariable("assessmentId") int assessmentId,
-                                                     @PathVariable("questionId") int questionId) {
+    public CompletableFuture<Boolean> removeQuestion(@PathVariable("assessmentId") int assessmentId,  @PathVariable("questionId") int questionId) {
         return svc.removeQuestion(assessmentId, questionId);
     }
 
     @PutMapping("/api/assessment/{assessmentId}/duration/{duration}")
-    public CompletableFuture<Boolean> changeDuration(@PathVariable("assessmentId") int assessmentId,
-                                                     @PathVariable("duration") String duration) {
+    public CompletableFuture<Boolean> changeDuration(@PathVariable("assessmentId") int assessmentId, @PathVariable("duration") String duration) {
         return svc.changeDuration(assessmentId, duration);
     }
 
     @PutMapping("/api/assessment/{assessmentId}/reschedule/{date}")
-    public CompletableFuture<Boolean> reschedule(@PathVariable("assessmentId") int assessmentId,
-                                                 @PathVariable("date") Date date) {
+    public CompletableFuture<Boolean> reschedule(@PathVariable("assessmentId") int assessmentId,@PathVariable("date") Date date) {
         return svc.reschedule(assessmentId, date);
     }
 
@@ -124,7 +136,7 @@ public class AssessmentController {
     @GetMapping("/api/assessment/getalltest/from/{fromDate}/to/{toDate}")
     public CompletableFuture<List<Test>> getAllTests(
             @PathVariable("fromDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date fromDate,
-            @PathVariable("toDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date toDate) {
+               @PathVariable("toDate") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date toDate) {
         return svc.getAllTests(fromDate, toDate);
     }
 
@@ -140,15 +152,13 @@ public class AssessmentController {
     }
 
     @PutMapping("/api/assessment/updatequestion/{questionId}")
-    public CompletableFuture<Boolean> updateQuestion(@PathVariable int questionId,
-                                                     @RequestBody Question question) {
+    public CompletableFuture<Boolean> updateQuestion(@PathVariable int questionId, @RequestBody Question question) {
         question.setId(questionId);
         return svc.updateQuestion(question);
     }
 
     @PutMapping("/api/assessment/updateteststatus/{testId}")
-    public CompletableFuture<Boolean> updateTestStatus(@PathVariable("testId") int testId,
-                                                       @RequestBody TestStatusUpdate status) {
+    public CompletableFuture<Boolean> updateTestStatus(@PathVariable("testId") int testId,  @RequestBody TestStatusUpdate status) {
         return svc.updateTestStatus(testId, status);
     }
 
