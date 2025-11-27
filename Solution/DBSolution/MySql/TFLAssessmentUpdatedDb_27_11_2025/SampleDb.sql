8956f0eec8cda169f730f215eac52bceb2a6662f
sanika-bhor <bhorsanika0239@gmail.com>
2025-11-27 15:51:48 +0530

    sample data for assessment

--- Solution/DBSolution/MySql/TFLAssessmentUpdatedDb_27_11_2025/SampleDb.sql
+++ Solution/DBSolution/MySql/TFLAssessmentUpdatedDb_27_11_2025/SampleDb.sql
@@ -338,18 +338,17 @@ INSERT INTO employeeperformance VALUES
 INSERT INTO user_session VALUES
 (1, 1, '2025-11-20 15:40:51', '2025-11-20 15:42:04', 'LOGGED_OUT');
 
+INSERT INTO Assessment 
+(test_id, candidate_id, status, createdby, createdon, schedulestart, scheduleend)
+VALUES
+(1, 4, 'Scheduled', 1, NOW(), '2025-01-20 10:00:00', '2025-01-20 12:00:00');
 
 INSERT INTO Assessment 
-(test_id, candidate_id, status, createdby, createdon)
+(test_id, candidate_id, status, createdby, createdon, schedulestart, scheduleend)
 VALUES
-(1,4 ,  'Scheduled', 1, NOW());
+(2, 4, 'Scheduled', 1, NOW(), '2025-01-22 09:00:00', '2025-01-22 11:00:00');
 
 INSERT INTO Assessment 
-(test_id, candidate_id,status, createdby, createdon)
+(test_id, candidate_id, status, createdby, createdon, schedulestart, scheduleend)
 VALUES
-(2,4,  'Scheduled', 1, NOW());
-
-INSERT INTO Assessment 
-(test_id, candidate_id,  status, createdby, createdon)
-VALUES
-(3,4, 'Scheduled', 1, NOW());
+(3, 4, 'Scheduled', 1, NOW(), '2025-01-25 14:00:00', '2025-01-25 16:00:00');
