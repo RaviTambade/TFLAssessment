9ebd2d3c14ba2f34367dd7c7f77b42cf06d59a74
Nikita Bansode <bansodenikita66@gmail.com>
2026-01-02 12:40:30 +0530

    Question Details Update

--- Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/entities/QuestionDetails.java
+++ Solution/WebSolution/Java/AssessmentAPI/src/main/java/com/transflower/tflassessment/entities/QuestionDetails.java
@@ -6,8 +6,15 @@ public class QuestionDetails {
 
     private int id;
     private String question;
+    private int questionId;
     private String subject;
     private String criteria;
+    private String A;
+    private String B;
+    private String C;
+    private String D;
+    private String result;
+
 
     public int getId() {
         return id;
@@ -25,6 +32,14 @@ public class QuestionDetails {
         this.question = question;
     }
 
+    public int getQuestionId(){
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId){
+        this.questionId=questionId;
+    }
+
     public String getSubject() {
         return subject;
     }
@@ -38,22 +53,69 @@ public class QuestionDetails {
     }
 
     public void setCriteria(String criteria) {
-        criteria = criteria;
+        this.criteria = criteria;
     }
+
+    public void setA(String A){
+        this.A=A;
+    }
+    public String getA(){
+        return A;
+    }
+
+    public void setB(String B){
+        this.B=B;
+    }
+    public String getB(){
+        return B;
+    }
+
+    public void setC(String C){
+        this.C=C;
+    }
+    public String getC(){
+        return C;
+    }
+
+    public void setD(String D){
+        this.D=D;
+    }
+    public String getD(){
+        return D;
+    }
+
+    public void setResult(String result){
+        this.result=result;
+    }
+
+    public String getResult(){
+        return result;
+    }
+
      public QuestionDetails() {
-        this.id = id;
-        this.question = question;
-        this.subject = subject;
-        this.criteria = criteria;
-
+        id = 0;
+        question = null;
+        questionId=0;
+        subject = null;
+        criteria = null;
+        A="";
+        B="";
+        C="";
+        D="";
+        result=null;
     }
 
-    public QuestionDetails(int id,String question,String subject,String criteria) {
+    public QuestionDetails(int id,String question,int questionId,String subject,String criteria,String A,String B,String C,String D,String result) {
         this.id = id;
         this.question = question;
+        this.questionId=questionId;
         this.subject = subject;
         this.criteria = criteria;
-
+        this.A=A;
+        this.B=B;
+        this.C=C;
+        this.D=D;
+        this.result=result;
     }
 
     @Override
@@ -85,7 +147,8 @@ public class QuestionDetails {
 
     @Override
     public String toString() {
-        return "QuestionDetails{id=" + id + ",question=" + question + "',subject=" + subject + ",criteria=" + criteria
+        return "QuestionDetails{id=" + id + ",question=" + question + "questionId="+questionId+"',subject=" + subject + ",criteria=" + criteria+
+                "A= "+A+"B= "+B+"C= "+C+"D= "+D+"Result = "+result
                 + "}";
     }
 
