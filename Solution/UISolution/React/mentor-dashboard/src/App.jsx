79df816b762bb8c62de41d88a59c0fb6e2f88545
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2026-01-15 16:00:20 +0530

    App.jsx file changes

--- Solution/UISolution/React/mentor-dashboard/src/App.jsx
+++ Solution/UISolution/React/mentor-dashboard/src/App.jsx
@@ -4,59 +4,42 @@ import SkillHealthSnapshot from "./components/SkillHealthSnapshot";
 import PublishAssessment from "./components/PublishAssessment";
 import MentorRecommendation from "./components/MentorRecommendation";
 
-
 import LearnerSkillAnalytics from "./components/LearnerSkillAnalytics";
 function App() {
   return (
     <>
-    <h2>MENTOR DATA</h2>
-    <div>
-      <MentorData />
-    </div>
-    <h2>TEST DATA</h2>
-    <div>
+      <h2>MENTOR DATA</h2>
+      <div>
+        <MentorData />
+      </div>
+      <h2>TEST DATA</h2>
+      <div>
         <TestData />
       </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
+      <div>
+        <h2>LEARNER SKILL ANALYTICS</h2>
+        <div>
+          <LearnerSkillAnalytics />
+        </div>
 
-    <h2>LEARNER SKILL ANALYTICS</h2>
-    <div>
-      <LearnerSkillAnalytics />
-    </div>  
-    
+        <h2>MENTOR ACTION DATA</h2>
+        <div>
+          <MentorRecommendation />
+        </div>
+
+        <h2>SKILL HEALTH SNAPSHOT</h2>
+        <div>
+          <SkillHealthSnapshot />
+        </div>
+
+        <h2>PUBLISH ASSESSMENT</h2>
+        <PublishAssessment />
+        <h2>Mentor Recommendation</h2>
+        <div>
+          <MentorRecommendation />
+        </div>
+      </div>
     </>
-
-
-=======
-<<<<<<< HEAD
-      <h2>MENTOR ACTION DATA</h2>
-      <div>
-        <MentorRecommendation/>
-      </div>
-      </>
-    
-=======
-=======
->>>>>>> c51a13ea15937fd40a01825cac8dc07b09d18571
-
-      <h2>SKILL HEALTH SNAPSHOT</h2>
-      <div>
-        <SkillHealthSnapshot/>
-      </div>
-
-      <h2>PUBLISH ASSESSMENT</h2>
-      <PublishAssessment />
-      <h2>Mentor Recommendation</h2>
-      <div>
-        <MentorRecommendation/>
-      </div>
-      </>    
-<<<<<<< HEAD
->>>>>>> d294a7c5caf271b44233125ddce5b0e6933d26d2
->>>>>>> 940cb8e027459769acc5958ce23618cab88d950b
-=======
->>>>>>> c51a13ea15937fd40a01825cac8dc07b09d18571
   );
 }
 
