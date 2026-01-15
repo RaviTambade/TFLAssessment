51496302d4c0a5e806b697962ee1e42ad0c4605a
rutujamokale <rutujamokale2003@gmail.com>
2026-01-15 14:59:51 +0530

    MentorRecommendation panel

--- Solution/UISolution/React/mentor-dashboard/src/App.jsx
+++ Solution/UISolution/React/mentor-dashboard/src/App.jsx
@@ -1,10 +1,7 @@
 import MentorData from "./components/MentorData";
 import TestData from "./components/TestData";
-<<<<<<< HEAD
-import MentorRecommendation from "./components/MentorRecommendation";
-=======
 import SkillHealthSnapshot from "./components/SkillHealthSnapshot";
->>>>>>> d294a7c5caf271b44233125ddce5b0e6933d26d2
+import MentorRecommendation from "./components/MentorRecommendation";
 
 function App() {
   return (
@@ -17,21 +14,16 @@ function App() {
     <div>
         <TestData />
       </div>
-<<<<<<< HEAD
-      <h2>MENTOR ACTION DATA</h2>
-      <div>
-        <MentorRecommendation/>
-      </div>
-      </>
-    
-=======
 
       <h2>SKILL HEALTH SNAPSHOT</h2>
       <div>
         <SkillHealthSnapshot/>
       </div>
+      <h2>Mentor Recommendation</h2>
+      <div>
+        <MentorRecommendation/>
+      </div>
       </>    
->>>>>>> d294a7c5caf271b44233125ddce5b0e6933d26d2
   );
 }
 
