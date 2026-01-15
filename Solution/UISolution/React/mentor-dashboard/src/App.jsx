60da33c5b51f21233c3a0c1b2222cb5246a58c6a
pranitamane25 <pranitamane2506@gmail.com>
2026-01-15 12:48:43 +0530

    SkillHealth

--- Solution/UISolution/React/mentor-dashboard/src/App.jsx
+++ Solution/UISolution/React/mentor-dashboard/src/App.jsx
@@ -1,5 +1,6 @@
 import MentorData from "./components/MentorData";
 import TestData from "./components/TestData";
+import SkillHealthSnapshot from "./components/SkillHealthSnapshot";
 
 function App() {
   return (
@@ -11,7 +12,13 @@ function App() {
     <h2>TEST DATA</h2>
     <div>
         <TestData />
-      </div></>
+      </div>
+
+      <h2>SKILL HEALTH SNAPSHOT</h2>
+      <div>
+        <SkillHealthSnapshot/>
+      </div>
+      </>    
   );
 }
 
---
+++ Solution/UISolution/React/mentor-dashboard/src/components/SkillHealthSnapshot.jsx
@@ -0,0 +1,40 @@
+import "./Design.css";
+
+function SkillHealthSnapshot(){
+
+    //hard coded list data
+    const skillHealth={
+        strong:"Programming Basics",
+        average: "Web Architecture",
+        weak: "Dependency Injection, LINQ" 
+
+    };
+
+    return (
+        <div className="SkillHealth-container">
+        
+        <div className="SkillHealth-title">
+             📊 Skill Health Snapshot
+            </div>
+
+        <div className="SkillHealth-list">
+            <div className="skill strong">
+            • <strong>Strong:</strong> {skillHealth.strong}
+        </div>
+
+        <div className="skill average">
+           • <strong>Average:</strong>{skillHealth.average}
+            </div>
+
+            <div className="skill Weak">
+           • <strong>Weak:</strong>{skillHealth.weak}
+            </div>
+
+        </div>
+        </div>
+            
+    );
+}
+export default SkillHealthSnapshot;
+            
+    
