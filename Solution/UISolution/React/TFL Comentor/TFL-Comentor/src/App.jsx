ecb7833a51ef7739e7a95e34d2bfb7a3b30a3624
Nikita Bansode <bansodenikita66@gmail.com>
2026-01-22 15:28:22 +0530

    Employer dashboard Service Implementation

--- Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/App.jsx
+++ Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
 import EmployerRoute from './components/route/employer/employerRoute'
-import './App.css'
 
 function App() {
 
   return (
-    <>
+
       <div>
        <EmployerRoute />
        </div>
-    </>
+
   )
 }
 
