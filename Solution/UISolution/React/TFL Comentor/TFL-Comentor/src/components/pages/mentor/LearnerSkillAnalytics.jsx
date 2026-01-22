d76ebb64258fd5aa364ba9f45412347197a04cbe
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-01-22 09:48:30 +0530

    pulling

--- Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/components/pages/mentor/LearnerSkillAnalytics.jsx
+++ Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/components/pages/mentor/LearnerSkillAnalytics.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from '../components/layout/Header';
-import MentorRecommendation from '../components/dashboard/LearnerSkillAnalytics';
+import MentorRecommendation from '../components/dashboard/mentor/LearnerSkillAnalytics';
 
 function LearnerSkillAnalytics() {
     return (
