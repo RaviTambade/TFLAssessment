a063a9a253f058ef9e81ee610550eab3255ba8f2
sanika-bhor <bhorsanika0239@gmail.com>
2026-01-24 17:33:27 +0530

    navigation menu for dahsboard

--- Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/components/route/student/studentRoute.jsx
+++ Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/components/route/student/studentRoute.jsx
@@ -1,15 +1,36 @@
-import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 
-import Sidebar from '../../layout/student/StudentSidebar';
+import StudentSidebar from "../../layout/student/StudentSidebar";
+import StudentDashboard from "../../pages/student/StudentDashboard";
+import Projects from "../../pages/student/ProjectsPage";
+import Assessments from "../../pages/student/AssessmentsPage";
+import MentorFeedback from "../../pages/student/MentorFeedbackPage";
+import SkillHealth from "../../pages/student/SkillHealthPage";
+import LearningPath from "../../pages/student/LearningPathPage";
+import CareerReadiness from "../../pages/student/CareerReadinessPage";
 
-const StudentRoute = () =>{
-     return (
-      <div className="d-flex">
-        <Sidebar />
-        <div className="flex-grow-1 p-3">
-          <Outlet/>
-        </div>
+const StudentRoute = () => {
+  return (
+    <div className="d-flex">
+      <StudentSidebar />
+      <div className="flex-grow-1 p-3">
+          <Routes>
+              {/* <Route path="/"  element={<StudentDashboard />} /> */}
+              <Route path="/dashboard" element={<StudentDashboard />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/assessments" element={<Assessments />} />
+              <Route path="/mentor-feedback" element={<MentorFeedback />} />
+              <Route path="/skill-health" element={<SkillHealth />} />
+              <Route path="/learning-path" element={<LearningPath />} />
+              <Route path="/career-readiness" element={<CareerReadiness />} />
+          </Routes>
       </div>
+    </div>
   );
 };
 
--- Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/main.jsx
+++ Solution/UISolution/React/TFL Comentor/TFL-Comentor/src/main.jsx
@@ -1,18 +1,12 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
