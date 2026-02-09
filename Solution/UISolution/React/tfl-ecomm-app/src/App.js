7c5d00f28a0775ec39789930605ebcab0cc67e33
Ravi Tambade <ravi.tambade@transflower.in>
2026-02-10 00:25:26 +0530

    Scope

---
+++ Solution/UISolution/React/tfl-ecomm-app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+
+import Product from './components/product';
+
+
+
+
+var  App=() => {
+
+
+  return (
+    <div className="App">
+        <Product /> 
+    </div>
+  );
+}
+
+export default App;
