7dea5c54f865a7af9731ef522af17509202c97d1
nirjalanaik1706 <nirjalanaik1706@gmail.com>
2026-02-27 12:43:01 +0530

    Spring boot application folder structure

---
+++ Solution/CoMentor/services/evaluation-content-service/src/main/java/com/tfl/content/EvaluationContentApplication.java
@@ -0,0 +1,12 @@
+package com.tfl.content;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvaluationContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvaluationContentApplication.class, args);
+	}
+
+}
