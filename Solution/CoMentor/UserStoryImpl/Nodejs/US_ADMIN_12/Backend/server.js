8f821f494ad8b7f627f3feeb1ab797e9cdb2114a
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2026-03-24 19:30:32 +0530

    view all users backend implemented

---
+++ Solution/CoMentor/UserStoryImpl/Nodejs/US_ADMIN_12/Backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const bodyparser =require('body-parser');
+
+const connection = require('./config/db');
+const userRepository = require('./repositories/userRepository');
+const UserService = require('./services/userService');
+const userController = require('./controllers/userController');
+const userRouterfactory = require('./routers/userRouter');
+
+const repo = new userRepository(connection);
+const service = new UserService(repo);
+const controller = new userController(service);
+const userRouter =userRouterfactory(controller);
+
+const app = express();
+app.use(cors());
+app.use(bodyparser.json());
+app.use('/api/users', userRouter);
+
+app.listen(4000);
+console.log("Server is running on port 4000");
