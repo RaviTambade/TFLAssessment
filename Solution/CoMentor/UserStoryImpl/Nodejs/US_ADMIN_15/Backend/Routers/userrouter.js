8ece7d0bc38f2c4d4f96041fd03d5f23424dc5ff
ARNAV <arnav.tolahunase21@gmail.com>
2026-03-26 15:09:45 +0530

    updated US_ADMIN_15

--- Solution/CoMentor/UserStoryImpl/Nodejs/US_ADMIN_15/Backend/Routers/userrouter.js
+++ Solution/CoMentor/UserStoryImpl/Nodejs/US_ADMIN_15/Backend/Routers/userrouter.js
@@ -4,9 +4,8 @@ module.exports = (controller) => {
     
   const router = express.Router();
 
-  router.put('/inactivateUser', controller.inactivateUser.bind(controller));
-  router.put('/blockUser', controller.blockUser.bind(controller));
-  router.put('/activateUser', controller.activateUser.bind(controller));
+  router.put('/changeUserStatus', controller.changeUserStatus.bind(controller));
+
 
   return router;
 };
