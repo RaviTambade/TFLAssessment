731be8d3a8efcf76d578ed072e9150a1847362cb
sai.jagdale <161121425+sai-jagdale@users.noreply.github.com>
2026-04-16 15:37:04 +0530

    Modifying Port no. of US_ADMIN_09

--- Solution/CoMentor/UserStoryImpl/Nodejs/US_ADMIN_09/backend/index.js
+++ Solution/CoMentor/UserStoryImpl/Nodejs/US_ADMIN_09/backend/index.js
@@ -21,7 +21,7 @@ const sessionRouter = sessionRouterFactory(sessionController);
 
 app.use('/api/admin/logs', sessionRouter);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
