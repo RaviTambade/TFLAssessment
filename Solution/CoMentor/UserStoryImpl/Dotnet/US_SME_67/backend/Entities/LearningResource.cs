6bbcd0a36ee073916f6b6260a95f7e4178f91110
sanikakulkarni21 <sanikakulkarni880@gmail.com>
2026-04-17 12:47:31 +0530

    implemented user_story_67 create test

---
+++ Solution/CoMentor/UserStoryImpl/Dotnet/US_SME_67/backend/Entities/LearningResource.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace backend.Entities;
+
+public partial class LearningResource
+{
+    public long Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public string? ResourceUrl { get; set; }
+
+    public string? Type { get; set; }
+
+    public long? UploadedBy { get; set; }
+
+    public string? Status { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual User? UploadedByNavigation { get; set; }
+}
