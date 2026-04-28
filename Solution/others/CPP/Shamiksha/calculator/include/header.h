477d910034012769f4b286c5f93350e6bd22cb05
Nikita Bansode <bansodenikita66@gmail.com>
2026-04-28 18:00:31 +0530

    changes

--- Solution/others/CPP/Shamiksha/calculator/include/header.h
+++
@@ -1,5 +0,0 @@
-   #include<stdio.h>
-float addition(float num1, float num2);
-float subtraction(float num1, float num2);
-float multiplication(float num1, float num2);
-float division (float num1, float num2);                 
