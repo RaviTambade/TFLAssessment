eae5bec1e4c7fe78c184756e8c9a4dffb704da0f
Ravi Tambade <ravi.tambade@transflower.in>
2026-05-01 02:34:32 +0530

    'changes'

--- Solution/CoMentor/Integrated/react/src/main.tsx
+++ Solution/CoMentor/Integrated/react/src/main.tsx
@@ -4,4 +4,13 @@ import App from './App.tsx'
 import './index.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+let theElement = document.getElementById("root");
+createRoot(theElement).render(<App />);
+
+
+//physical path  //C:\Users\Tansflower\Documents\GitHub\CoMentor\Integrated\react\src\main.tsx
+//virtual path  //src/main.tsx
+
+// url path  //http://localhost:8080/src/main.tsx
+
+//https://tansflowerlearning.com
