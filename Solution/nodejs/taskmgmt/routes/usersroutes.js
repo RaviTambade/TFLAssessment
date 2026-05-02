1b3e7d42330a695872281888a69ca52d33943c34
Sachin Kharat <kharatsachin012@gmail.com>
2026-05-02 17:29:37 +0530

    Remove unwantend comments

--- Solution/nodejs/taskmgmt/routes/usersroutes.js
+++ Solution/nodejs/taskmgmt/routes/usersroutes.js
@@ -3,11 +3,11 @@ const express = require('express');
 module.exports = (controller) => {
     const router = express.Router();
 
-    router.get('/getAllUsers', controller.getAllUsers.bind(controller)); //done //http://localhost:5000/api/tasks/createTask
-    router.get('/getUserById/:id', controller.getUserById.bind(controller)); //done //http://localhost:5000/api/tasks/getTaskById/1
-    router.post('/createUser', controller.createUser.bind(controller)); //done //http://localhost:5000/api/tasks/getTaskByCreator/1
-    router.put('/updateUser/:id', controller.updateUser.bind(controller)); //done //http://localhost:5000/api/tasks/getTasksByDate/2026-04-29/2026-04-30
-    router.delete('/deleteUser/:id', controller.deleteUser.bind(controller)); //done //http://localhost:5000/api/tasks/getParentTask/3
+    router.get('/getAllUsers', controller.getAllUsers.bind(controller));
+    router.get('/getUserById/:id', controller.getUserById.bind(controller));
+    router.post('/createUser', controller.createUser.bind(controller));
+    router.put('/updateUser/:id', controller.updateUser.bind(controller));
+    router.delete('/deleteUser/:id', controller.deleteUser.bind(controller));
 
     return router;
 }
