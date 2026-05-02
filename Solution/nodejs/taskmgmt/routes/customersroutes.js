56d284f1c7f119455516a43d32fcfb909d610800
rahulgayke1704 <rahulgayke1704@gmail.com>
2026-05-02 16:38:12 +0530

    customer

--- Solution/nodejs/taskmgmt/routes/customersroutes.js
+++ Solution/nodejs/taskmgmt/routes/customersroutes.js
@@ -4,14 +4,11 @@ module.exports = (controller) => {
     const router = express.Router();
 
     router.post('/createTask', controller.createTask.bind(controller));
-    router.get('/getAllTasks', controller.getAllTasks.bind(controller));
-    router.get('/getTaskById/:id', controller.getTaskById.bind(controller));
-    router.get('/getTaskByCreator/:user_id', controller.getTaskByCreator.bind(controller));
-    router.get('/getTasksByDate/:fromDate/:toDate', controller.getTasksByDate.bind(controller));
-    router.get('/getParentTask/:id', controller.getParentTask.bind(controller));
-    router.put('/updateTask/:id', controller.updateTask.bind(controller));
-    router.patch('/deleteTask/:id', controller.deleteTask.bind(controller));
-    router.get('/getAllocatedTasksByStudentId/:id', controller.getAllocatedTasksByStudentId.bind(controller));
-
+   router.get('/getAllCustomers', controller.getAllCustomers.bind(controller));
+    router.get('/getCustomerById/:id', controller.getCustomerById.bind(controller));
+    router.post('/createCustomer/:user_id', controller.createCustomer.bind(controller));
+    router.put('/updateCustomer/:fromDate/:toDate', controller.updateCustomer.bind(controller));
+    router.delete('/deleteCustomer/:id', controller.deleteCustomer.bind(controller));
+    
     return router;
 }
