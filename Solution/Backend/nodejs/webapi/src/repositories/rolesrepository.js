c25560c6fa27b391f9c6e68888cb024c574b91fa
Sumit Sudam Bhor <sumitbhor227@gmail.com>
2026-05-14 11:06:45 +0530

    restructuring node js code

--- Solution/Backend/nodejs/webapi/src/repositories/rolesrepository.js
+++ Solution/Backend/nodejs/webapi/src/repositories/rolesrepository.js
@@ -8,43 +8,36 @@ class RolesRepository {
     this.connection.query(sql, callback);
   }
 
-  addNewRole(newrole, callback) {
+  insert(newRole, callback) {
     const sql = "insert into roles(role_name,description) values(?,?)";
-    this.connection.query(
-      sql,
-      [newrole.roleName, newrole.description],
-      callback,
-    );
+    this.connection.query(sql,[newRole.roleName, newRole.description],callback);
   }
 
-  updateExistingRole(roleId, role, callback) {
+  update(roleId, role, callback) {
     const sql = "UPDATE roles SET role_name=?, description=? WHERE role_id=?";
-    this.connection.query(
-      sql,
-      [role.roleName, role.description, roleId],
-      callback,
-    );
+    this.connection.query(sql, [role.roleName, role.description, roleId],callback);
   }
 
   getRoleById(roleId, callback) {
     const sql = "select * from roles where role_id =?";
-    this.connection.query(sql, [roleId], callback);
+    this.connection.query(sql,[roleId], callback);
   }
 
-  getUserByRole(userId, callback) {
-    const query = `SELECT r.role_name FROM user_roles ur
-                  JOIN roles r ON ur.role_id = r.role_id
-                  WHERE ur.user_id = ? `;
+  /**************** changes expected node js team for getting roleid, rolename*********** */
+  getRoleByUserId(userId, callback) {
+    const query = `SELECT  ur.role_id, r.role_name 
+                  FROM user_roles ur join roles r  
+                  ON ur.role_id = r.role_id
+                  WHERE user_id = ?; `;
 
     this.connection.query(query, [userId], (err, results) => {
       if (err) return callback(err, null);
-
       const userRole = results.length > 0 ? results : null;
       callback(null, userRole);
     });
   }
 
-  getUserRoleIds(userId, callback) {
+  getUserRoleIdsByUserId(userId, callback) {
     const query = `SELECT role_id FROM user_roles WHERE user_id = ?`;
     this.connection.query(query, [userId], callback);
   }
@@ -54,7 +47,7 @@ class RolesRepository {
     this.connection.query(deleteSql, [userId, roleId], callback);
   }
 
-  unAssignRoles(userId, callback) {
+  unAssignAllRolesToUserId(userId, callback) {
     const deleteSql = "DELETE FROM user_roles WHERE user_id = ?";
     this.connection.query(deleteSql, [userId], callback);
   }
