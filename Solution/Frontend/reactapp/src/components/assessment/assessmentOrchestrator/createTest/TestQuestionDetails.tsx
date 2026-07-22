95bbcef06f321bbc8d922575b1a33f6c3d9ac3bd
pranitamane25 <pranitamane2506@gmail.com>
2026-07-22 15:43:46 +0530

    GetQuestionDetails component added

--- Solution/Frontend/reactapp/src/components/assessment/assessmentOrchestrator/createTest/TestQuestionDetails.tsx
+++ Solution/Frontend/reactapp/src/components/assessment/assessmentOrchestrator/createTest/TestQuestionDetails.tsx
@@ -56,7 +56,7 @@ export default function TestQuestionDetails() {
           className="flex items-center gap-2 rounded-lg border border-gray-200 bg-white px-4 py-2 text-sm font-semibold text-gray-700 shadow-sm hover:bg-gray-100 transition-colors"
         >
           <ArrowLeft className="h-4 w-4" />
-          Back to pavellion
+          Back 
         </button>
       </div>
 
