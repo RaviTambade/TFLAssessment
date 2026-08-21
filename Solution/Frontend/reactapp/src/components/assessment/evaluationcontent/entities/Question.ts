dfb87a459fdd169c689d9b9574a6669649ba711e
Tanvi Sonawane <tanvi.s1804@gmail.com>
2026-08-21 15:43:06 +0530

    Indentation

---
+++ Solution/Frontend/reactapp/src/components/assessment/evaluationcontent/entities/Question.ts
@@ -0,0 +1,13 @@
+interface Question {
+  questionId: number;
+  description: string;
+  questionType: string;
+  difficultyLevel: string;
+  createdAt: string;
+  status: string;
+  language: string;
+  layer: string;
+  framework: string;
+  concept: string;
+};
+export default Question;
